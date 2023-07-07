e04024df21b5791f3d5af1ed0a022a6713896cb2
makrandshinde <makrand.shinde@ayanworks.com>
2023-07-07 14:51:59 +0530

    done signIn and signUp API integration
    Signed-off-by: makrandshinde <makrand.shinde@ayanworks.com>

--- src/config/CommonConstant.ts
+++ src/config/CommonConstant.ts
@@ -1,1 +1,9 @@
 export const passwordRegex = /^(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z])(?=.*[-!@$%^*])(?=.*[!"$%*,-.\/:;=@^_])[a-zA-Z0-9!"$%*,-.\/:;=@^_]{8,}$/ 
+
+export const apiStatusCodes = {
+    API_STATUS_SUCCESS : 200,
+    API_STATUS_CREATED : 201,
+    API_STATUS_BAD_REQUEST : 400,
+    API_STATUS_UNAUTHORIZED : 401,
+    API_STATUS_NOT_FOUND : 404
+}
