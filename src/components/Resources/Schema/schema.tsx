889a612a3640168f464189a011e148378121aedf
Tipu Singh <tipu.singh@ayanworks.com>
2023-07-07 11:55:23 +0530

    Added sidebar in schema
    Signed-off-by: Tipu Singh <tipu.singh@ayanworks.com>

---
+++ src/components/Resources/Schema/schema.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import { Card, Table, } from 'flowbite-react';
+
+
+const SchemaTable = () => {
+  return (
+   
+      <h6>
+        Schema Listhllllllljjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
+      </h6>
+  
+
+  
+
+  )
+}
+
+
+export default SchemaTable 
