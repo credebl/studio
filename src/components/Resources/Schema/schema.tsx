8a8e9f77c16cea60ab1fac31c4aec574cbdac854
Tipu Singh <tipu.singh@ayanworks.com>
2023-07-11 18:55:37 +0530

    Added create schema page
    Signed-off-by: Tipu Singh <tipu.singh@ayanworks.com>

--- src/components/Resources/Schema/schema.tsx
+++ src/components/Resources/Schema/schema.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import type { AxiosResponse } from 'axios';
-import { Card, Table, } from 'flowbite-react';
+import { Button, Card, Table, } from 'flowbite-react';
 import { useEffect, useState } from 'react';
 import { getAllSchemas } from '../../../api/Schema';
 import SchemaCard from '../../../commonComponents/schemaCard';
@@ -22,15 +22,25 @@ const SchemaTable = () => {
 
   return (
     <>
-    <div>
-    <h1 className='p-4 font-bold underline'>Schema List</h1>
-    </div>
+      <div>
+        <h1 className='p-4 font-bold underline'>Schema List</h1>
+        <div className="flex items-center justify-end mb-4 p-2">
+            <Button
+              onClick={() => {
+                window.location.href = '/resources/create'
+              }}
+              className='text-base font-medium text-center text-white bg-primary-700 rounded-lg hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 sm:w-auto dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"'
+            >
+              Create Schema
+            </Button>
+          </div>
+      </div>
       <div style={{ display: 'flex', flexDirection: 'column' }}>
         <div style={{ display: 'grid', gridTemplateColumns: 'repeat(auto-fit, minmax(500px, 1fr))', gap: '1rem' }}>
           {schemaList && schemaList.length > 0 &&
             schemaList.map((element, key) => (
               <div className='p-4' key={key}>
-                <SchemaCard schemaName={element['name']} version={element['version']} schemaId = {element['schemaLedgerId']} issuerDid = {element['issuerId']} attributes = {element['attributes']}/>
+                <SchemaCard schemaName={element['name']} version={element['version']} schemaId={element['schemaLedgerId']} issuerDid={element['issuerId']} attributes={element['attributes']} />
               </div>
             ))}
         </div>
