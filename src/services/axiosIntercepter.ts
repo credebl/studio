09e0ffaba688e3195f08d0103f1f6a700f7f1928
makrandshinde <makrand.shinde@ayanworks.com>
2023-07-14 12:36:44 +0530

    merged with main
    Signed-off-by: makrandshinde <makrand.shinde@ayanworks.com>

--- src/services/axiosIntercepter.ts
+++ src/services/axiosIntercepter.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const instance = axios.create({
-    baseURL: import.meta.env.PUBLIC_BASE_URL ?? process.env.PUBLIC_BASE_URL
+    baseURL: import.meta.env.PUBLIC_BASE_URL || import.meta.env.MYURL
 })
 
 export default instance
