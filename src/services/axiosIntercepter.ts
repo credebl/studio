1ab9caca68e54bb7360cd2957a3ded37908e72b7
makrandshinde <makrand.shinde@ayanworks.com>
2023-07-14 11:42:12 +0530

    setup env vars
    Signed-off-by: makrandshinde <makrand.shinde@ayanworks.com>

--- src/services/axiosIntercepter.ts
+++ src/services/axiosIntercepter.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const instance = axios.create({
-    baseURL: `${import.meta.env.PUBLIC_BASE_URL}`
+    baseURL: import.meta.env.PUBLIC_BASE_URL ?? process.env.PUBLIC_BASE_URL
 })
 
 export default instance
