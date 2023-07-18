aceb3144f6c3cfa14001978e3bdccfbcee9ac05a
@nishad.shirsat <nishad.shirsat@ayanworks.com>
2023-07-18 15:36:22 +0530

    removed middleware.ts file
    Signed-off-by: @nishad.shirsat <nishad.shirsat@ayanworks.com>

--- src/middleware.ts
+++
@@ -1,8 +0,0 @@
-import { defineMiddleware } from 'astro/middleware';
-
-export const onRequest = defineMiddleware(async (_, next) => {
-    console.log('Middleware TEST');
-    const response = await next();
-
-    return response;
-});
