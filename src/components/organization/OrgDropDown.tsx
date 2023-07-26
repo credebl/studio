0dcb84c77fdc5f6c47ff6d8be98f0a7a814f5077
pranalidhanavade <pranali.dhanavade@ayanworks.com>
2023-07-26 19:05:46 +0530

    worked on create organization in dropdown

--- src/components/organization/OrgDropDown.tsx
+++ src/components/organization/OrgDropDown.tsx
@@ -9,35 +9,42 @@ import { BiChevronDown } from "react-icons/bi";
 
 const OrgDropDown = () => {
 	const [orgList, setOrgList] = useState<Organisation[]>([]);
-    const [activeOrg, setactiveOrg] = useState<Organisation | null>(null)
+	const [activeOrg, setactiveOrg] = useState<Organisation | null>(null)
+	const [showModal, setShowModal] = useState(false);
+
 
 	useEffect(() => {
-        getAllorgs()
-    }, []);
+		getAllorgs()
+	}, []);
 
-    const getAllorgs = async () => {
-			const response = await getOrganizations(1, 10, '');
-			const { data } = response as AxiosResponse;
-			if (data?.statusCode === apiStatusCodes.API_STATUS_SUCCESS) {
-				setOrgList(data?.data?.organizations);
-                setActiveOrg(data?.data?.organizations)
-			} else {
-                console.log("data?.data?.organizations")
-			}
-		};
-
-    const goToOrgDashboard = async (orgId: number) => {
-			await setToLocalStorage(storageKeys.ORG_ID, orgId.toString());
-			window.location.href = '/organizations/dashboard';
+	const getAllorgs = async () => {
+		const response = await getOrganizations(1, 10, '');
+		const { data } = response as AxiosResponse;
+		if (data?.statusCode === apiStatusCodes.API_STATUS_SUCCESS) {
+			setOrgList(data?.data?.organizations);
+			setActiveOrg(data?.data?.organizations)
+		} else {
+			console.log("data?.data?.organizations")
+		}
 	};
 
-    const setActiveOrg = async(organizations:Organisation[]) => {
-        const orgId = await getFromLocalStorage(storageKeys.ORG_ID)
-        if(orgId){
-            const activeOrg = organizations?.find(org => org.id === Number(orgId))
-            setactiveOrg(activeOrg || null)
-        }
-    }
+	const goToOrgDashboard = async (orgId: number) => {
+		await setToLocalStorage(storageKeys.ORG_ID, orgId.toString());
+		window.location.href = '/organizations/dashboard';
+	};
+
+	const setActiveOrg = async (organizations: Organisation[]) => {
+		const orgId = await getFromLocalStorage(storageKeys.ORG_ID)
+		if (orgId) {
+			const activeOrg = organizations?.find(org => org.id === Number(orgId))
+			setactiveOrg(activeOrg || null)
+		}
+	}
+
+	const redirectToCreateOrgModal = () => {
+		window.location.href = '/organizations?orgModal=true';
+		setShowModal(true);
+	}
 
 	return (
 		<>
@@ -45,29 +52,29 @@ const OrgDropDown = () => {
 				id="dropdownUsersButton"
 				data-dropdown-toggle="dropdownUsers"
 				data-dropdown-placement="bottom"
-				className="text-white text-lg h-10 bg-blue-700 cursor-pointer  focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium 
+				className="text-white text-lg h-10 bg-blue-700 cursor-pointer focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium 
 					rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
 			>
-				
+
 				<>
-                {
-                    activeOrg ?
-                    <>
-                    {activeOrg.logoUrl ? (
-										<CustomAvatar size="20" src={activeOrg?.logoUrl} round />
-									) : (
-										<CustomAvatar size="20" name={activeOrg?.name} round />
-									)}
-                    <text className="ml-2">{activeOrg?.name}</text>
-                    </>
-                    :
-                    <text>
-					Select organization
-				    </text>
-                }
-                </>
-           
-				<BiChevronDown size={25} color='white'/>
+					{
+						activeOrg ?
+							<>
+								{activeOrg.logoUrl ? (
+									<CustomAvatar size="20" src={activeOrg?.logoUrl} round />
+								) : (
+									<CustomAvatar size="20" name={activeOrg?.name} round />
+								)}
+								<text className="ml-2">{activeOrg?.name}</text>
+							</>
+							:
+							<text>
+								Select organization
+							</text>
+					}
+				</>
+
+				<BiChevronDown size={25} color='white' />
 			</div>
 			<div
 				id="dropdownUsers"
@@ -104,6 +111,7 @@ const OrgDropDown = () => {
 				<a
 					href="#"
 					className="flex items-center p-3 text-sm font-medium text-blue-600 border-t border-gray-200 rounded-b-lg bg-gray-50 dark:border-gray-600 hover:bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:text-blue-500 hover:underline"
+					onClick={redirectToCreateOrgModal}
 				>
 					<svg
 						className="w-4 h-4 mr-2"
