587de0db5beab51a3d33e5dc00d57780f69d2cf3
Girish9960 <girish.patil@ayanworks.com>
2023-07-27 17:04:32 +0530

    fix: Change ICONs, Color codes, Button Names
    Signed-off-by: Girish9960 <girish.patil@ayanworks.com>

--- src/components/organization/Users.tsx
+++ src/components/organization/Users.tsx
@@ -90,7 +90,7 @@ const Users = () => {
                     <div className="inline-flex items-center w-auto xl:w-full 2xl:w-auto">
                     <button type="button" className="text-white w-full inline-flex items-center justify-center bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
             <svg aria-hidden="true" className="mr-1 -ml-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z"></path><path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd"></path></svg>
-            Role
+            Edit
         </button> 
                     </div>
                   </div>
@@ -129,7 +129,7 @@ const Users = () => {
                     <div className="inline-flex items-center w-auto xl:w-full 2xl:w-auto">
                     <button type="button" className="text-white w-full inline-flex items-center justify-center bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
             <svg aria-hidden="true" className="mr-1 -ml-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z"></path><path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd"></path></svg>
-            Role
+            Edit
         </button> 
                     </div>
                   </div>
