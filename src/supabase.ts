a95a27eccd147ce6090cd83e7a966fefc218f3a9
@nishad.shirsat <nishad.shirsat@ayanworks.com>
2023-07-31 11:56:00 +0530

    Removed unnecessary files and code
    Signed-off-by: @nishad.shirsat <nishad.shirsat@ayanworks.com>

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,6 +1,5 @@
 // import type { Database } from './types'
 import { createClient } from '@supabase/supabase-js'
-// const supabaseUrl = import.meta.env.SUPABASE_URL
-const supabaseUrl = 'vsgcvsd chgdsvchgsd'
-const supabaseAnonKey = 'dcgjevcjgewvbkbrfybwbgVVJVhgvjcvYVcfcgfCGFCGFc'
+const supabaseUrl = import.meta.env.SUPABASE_URL
+const supabaseAnonKey = import.meta.env.SUPABASE_ANON_KEY
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
