9d6653ce22f95e7b126ffabdf4744da217b60537
@nishad.shirsat <nishad.shirsat@ayanworks.com>
2023-07-31 22:23:32 +0530

    Worked on the user dashboard
    Signed-off-by: @nishad.shirsat <nishad.shirsat@ayanworks.com>

--- src/components/UserDashBoard.tsx
+++ src/components/UserDashBoard.tsx
@@ -1,11 +1,114 @@
+import type { AxiosResponse } from "axios";
+import { useEffect, useState } from "react";
+import { getUserInvitations } from "../api/invitations";
+import { getOrganizations } from "../api/organization";
+import { apiStatusCodes } from "../config/CommonConstant";
+import { pathRoutes } from "../config/pathRoutes";
+import { AlertComponent } from "./AlertComponent";
+import type { Organisation } from "./organization/interfaces";
+import CustomAvatar from '../components/Avatar'
 
-	const UserDashBoard = () => {
+const initialPageState = {
+	pageNumber: 1,
+	pageSize: 10,
+	total: 0,
+};
+
+const UserDashBoard = () => {
+
+	const [message, setMessage] = useState<string | null>(null)
+	const [error, setError] = useState<string | null>(null)
+	const [currentPage, setCurrentPage] = useState(initialPageState);
+	const [loading, setLoading] = useState<boolean>(false)
+	const [organizationsList, setOrganizationList] = useState<Array<Organisation> | null>(null)
+
+
+	const getAllInvitations = async () => {
+
+		setLoading(true)
+		const response = await getUserInvitations(currentPage.pageNumber, currentPage.pageSize, '');
+		const { data } = response as AxiosResponse
+
+		if (data?.statusCode === apiStatusCodes.API_STATUS_SUCCESS) {
+
+			const totalPages = data?.data?.totalPages;
+
+			const invitationList = data?.data?.invitations
+			if (invitationList.length === 0) {
+				setError('No Data Found')
+			}
+
+			if (invitationList.length > 0) {
+				setMessage('You have some pending received invitations')
+			}
+
+			setCurrentPage({
+				...currentPage,
+				total: totalPages
+			})
+		} else {
+			setError(response as string)
+		}
+
+		setLoading(false)
+	}
+
+	//Fetch the user organization list
+	const getAllOrganizations = async () => {
+
+		setLoading(true)
+		const response = await getOrganizations(currentPage.pageNumber, currentPage.pageSize, '');
+		const { data } = response as AxiosResponse
+
+		if (data?.statusCode === apiStatusCodes.API_STATUS_SUCCESS) {
+
+			const totalPages = data?.data?.totalPages;
+
+			const orgList = data?.data?.organizations.filter((userOrg: Organisation, index: number) => index < 3)
+
+			if (orgList.length === 0) {
+				setError('No Data Found')
+			}
+
+			setOrganizationList(orgList)
+			setCurrentPage({
+				...currentPage,
+				total: totalPages
+			})
+		} else {
+			setError(response as string)
+		}
+
+		setLoading(false)
+	}
+
+
+	useEffect(() => {
+		getAllInvitations()
+		getAllOrganizations()
+	}, [])
+
+	const redirectToInvitations = () => {
+		window.location.href = pathRoutes.users.invitations
+	}
 
 	return (
 		<div className="px-4 pt-6">
+			<div className="cursor-pointer" onClick={redirectToInvitations}>
+				<AlertComponent
+					message={message ? message : error}
+					type={message ? 'warning' : 'failure'}
+					onAlertClose={() => {
+						setMessage(null)
+						setError(null)
+					}}
+				/>
+			</div>
+
 			<div
 				className="grid w-full grid-cols-1 gap-4 mt-0 mb-4 xl:grid-cols-2 2xl:grid-cols-3"
 			>
+
 				<div
 					className=" justify-between p-4 bg-white border border-gray-200 rounded-lg shadow-sm sm:flex dark:border-gray-700 sm:p-6 dark:bg-gray-800"
 				>
@@ -13,36 +116,23 @@
 						<h2 className="text-base font-bold text-gray-500 dark:text-gray-400">
 							Organizations
 						</h2>
-						<div className="pt-4 flex">
-							<img
-								className="w-6 h-6 rounded-full "
-								src="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
-								alt="user photo"
-							/>
-							<h3 className="pl-2.5"> AyanWorks</h3>
-						</div>
 
-						<div className="pt-4 flex">
-							<img
-								className="w-6 h-6 rounded-full "
-								src="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
-								alt="user photo"
-							/>
-							<h3 className="pl-2.5">Blockster</h3>
-						</div>
+						{
+							organizationsList
+							&& organizationsList.map(org => {
+								return <div className="pt-4 flex">
 
-						<div className="pt-4 flex">
-							<img
-								className="w-6 h-6 rounded-full "
-								src="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
-								alt="user photo"
-							/>
-							<h3 className="pl-2.5">Health</h3>
+									{(org.logoUrl) ? <CustomAvatar size='30' src={org.logoUrl} /> : <CustomAvatar size='30' name={org.name} />}
 
-						</div>
+									<h3 className="pl-2.5"> {org.name}</h3>
+								</div>
+							})
+						}
+
+
 						<a
-							href="#"
-							className="pl-60 inline-flex items-center p-2 text-sm font-medium rounded-lg text-primary-700 hover:bg-gray-100 dark:text-primary-500 dark:hover:bg-gray-700"
+							href="/organizations"
+							className="float-right inline-flex items-center p-2 text-sm font-medium rounded-lg text-primary-700 hover:bg-gray-100 dark:text-primary-500 dark:hover:bg-gray-700"
 						>
 							View More..
 						</a>
@@ -57,40 +147,9 @@
 						<h3 className="text-base font-bold text-gray-500 dark:text-gray-400">
 							Ecosystems
 						</h3>
-
-						<div className="pt-4 flex">
-							<img
-								className="w-6 h-6 rounded-full "
-								src="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
-								alt="user photo"
-							/>
-							<h3 className="pl-2.5"> Lorem Ipsum</h3>
-						</div>
-
-						<div className="pt-4 flex">
-							<img
-								className="w-6 h-6 rounded-full "
-								src="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
-								alt="user photo"
-							/>
-							<h3 className="pl-2.5">Lorem Ipsum</h3>
-						</div>
-
-						<div className="pt-4 flex">
-							<img
-								className="w-6 h-6 rounded-full "
-								src="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
-								alt="user photo"
-							/>
-							<h3 className="pl-2.5">Lorem Ipsum</h3>
-						</div>
-
-						<a
-							href="#"
-							className="pl-60 inline-flex items-center p-2 text-sm font-medium rounded-lg text-primary-700 hover:bg-gray-100 dark:text-primary-500 dark:hover:bg-gray-700"
-						>
-							View More..
-						</a>
+						<h1
+							className="w-100 mt-8 text-center p-2 text-xl font-medium rounded-lg text-primary-700 dark:text-primary-500 dark:hover:bg-gray-700"
+						>Coming Soon..</h1>
 					</div>
 
 				</div>
@@ -101,38 +160,10 @@
 						<h3 className="mb-2 text-base font-bold text-gray-500 dark:text-gray-400">
 							DID Methods
 						</h3>
-						<div className="pt-4 flex">
-							<img
-								className="w-6 h-6 rounded-full "
-								src="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
-								alt="user photo"
-							/>
-							<h3 className="pl-2.5">DID:Indy</h3>
-						</div>
 
-						<div className="pt-4 flex">
-							<img
-								className="w-6 h-6 rounded-full "
-								src="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
-								alt="user photo"
-							/>
-							<h3 className="pl-2.5">DID:Key</h3>
-						</div>
-
-						<div className="pt-4 flex">
-							<img
-								className="w-6 h-6 rounded-full "
-								src="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
-								alt="user photo"
-							/>
-							<h3 className="pl-2.5">DID:Mob</h3>
-						</div>
-						<a
-							href="#"
-							className="pl-60 inline-flex items-center p-2 text-sm font-medium rounded-lg text-primary-700 hover:bg-gray-100 dark:text-primary-500 dark:hover:bg-gray-700"
-						>
-							Configure
-						</a>
+						<h1
+							className="w-100 mt-8 text-center p-2 text-xl font-medium rounded-lg text-primary-700 dark:text-primary-500 dark:hover:bg-gray-700"
+						>Coming Soon..</h1>
 					</div>
 					<div id="traffic-channels-chart" className="w-full"></div>
 				</div>
@@ -215,10 +246,9 @@
 						</li>
 					</ol>
 				</div>
-				{/* Carousel widget */}
 
 			</div>
-			
+
 		</div>
 
 	)
