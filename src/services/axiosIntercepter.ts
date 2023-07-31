3aae701b9b8567ff8a789d0510f84f86038ff0d7
@nishad.shirsat <nishad.shirsat@ayanworks.com>
2023-07-31 11:43:29 +0530

    worked on the user authentication
    Signed-off-by: @nishad.shirsat <nishad.shirsat@ayanworks.com>

--- src/services/axiosIntercepter.ts
+++ src/services/axiosIntercepter.ts
@@ -10,12 +10,13 @@ instance.interceptors.response.use(function (response) {
     // Any status code that lie within the range of 2xx cause this function to trigger
     // Do something with response data
     return response;
-}, function (error) {
+}, async function (error) {
     // Any status codes that falls outside the range of 2xx cause this function to trigger
     // Do something with response error
     const errorRes = error?.response;
 
     if(errorRes?.status === 401){
+        await localStorage.clear()
         window.location.href = pathRoutes.auth.sinIn
     }
     
