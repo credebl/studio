2f57b4b9332508fd775957947d937f1afe2effab
@nishad.shirsat <nishad.shirsat@ayanworks.com>
2023-08-03 16:24:27 +0530

    fix: dark theme text color change in empty state component
    Signed-off-by: @nishad.shirsat <nishad.shirsat@ayanworks.com>

--- src/components/EmptyListComponent/index.tsx
+++ src/components/EmptyListComponent/index.tsx
@@ -1,4 +1,4 @@
-import { ReactElement } from "react";
+import type { ReactElement } from "react";
 
 export const EmptyListMessage = ({ message, description, buttonContent, svgComponent, onClick }
     : {
@@ -11,12 +11,12 @@ export const EmptyListMessage = ({ message, description, buttonContent, svgCompo
     }) => {
     return (
         <div className='flex mt-20 justify-start items-center flex-col'>
-            <p className='text-2xl font-bold mb-4'>{message}</p>
-            <p className='text-lg mb-4'>{description}</p>
+            <p className='text-2xl font-bold mb-4 text-gray-900 dark:text-white'>{message}</p>
+            <p className='text-lg mb-4 text-gray-900 dark:text-white'>{description}</p>
             {
                 buttonContent
                 && <button
-                    className='group flex h-min p-3 mt-10 items-center justify-center p-0.5 font-medium focus:z-10 border border-transparent enabled:hover:bg-cyan-800 dark:enabled:hover:bg-cyan-700 text-base font- text-center text-white bg-primary-700 rounded-lg hover:bg-accent-00 focus:ring-4 focus:ring-primary-300 sm:w-auto dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800&quot;'
+                    className='group flex h-min p-3 mt-5 items-center justify-center p-0.5 font-medium focus:z-10 border border-transparent enabled:hover:bg-cyan-800 dark:enabled:hover:bg-cyan-700 text-base font- text-center text-white bg-primary-700 rounded-lg hover:bg-accent-00 focus:ring-4 focus:ring-primary-300 sm:w-auto dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800&quot;'
                     onClick={onClick}>
                     {svgComponent}
                     <span className="ml-2">{buttonContent}</span>
