d763d77de18e1eb4a10cf9e014ee2dd7fb1238ff
Yogesh Pawar <yogesh.pawar@ayanworks.com>
2023-08-04 15:04:15 +0530

    feat: Implemented Schema selection functionality in Issuance flow
    Signed-off-by: Krishna Waske <krishna.waske@ayanworks.com>

--- src/utils/TextTransform.ts
+++ src/utils/TextTransform.ts
@@ -1,5 +1,9 @@
-
-
 export const TextTittlecase = (text: string): string => {
-    return text.charAt(0).toUpperCase() + text.slice(1)
-}
+	return text.charAt(0).toUpperCase() + text.slice(1);
+};
+
+export const copyText = (copiedText: string | undefined) => {
+	if (copiedText) {
+		navigator.clipboard.writeText(copiedText);
+	}
+};
