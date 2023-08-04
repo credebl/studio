f1a26d6a09f9d624e10836e26e452945fa24b5f4
@nishad.shirsat <nishad.shirsat@ayanworks.com>
2023-08-04 19:46:13 +0530

    Worked on the deno env deploy changes
    Signed-off-by: @nishad.shirsat <nishad.shirsat@ayanworks.com>

---
+++ src/config/envConfig.ts
@@ -0,0 +1,6 @@
+
+export const envConfig = {
+    PUBLIC_BASE_URL: globalThis.baseUrl,
+    PUBLIC_CRYPTO_PRIVATE_KEY: globalThis.encryptKey
+
+}
---
+++ src/pages/server.ts
@@ -0,0 +1,1 @@
+import "https://deno.land/std@0.197.0/dotenv/load.ts";
