193b2d24f2d7f76ca9fb913cf368e293b1b68efc
Yogesh Pawar <yogesh.pawar@ayanworks.com>
2023-08-04 13:49:27 +0530

    feat: Displayed issued credential list
    Signed-off-by: Yogesh Pawar <yogesh.pawar@ayanworks.com>

--- src/config/apiRoutes.ts
+++ src/config/apiRoutes.ts
@@ -42,5 +42,8 @@ export const apiRoutes = {
         fidoAuthentication: 'Fido/generate-authentication-options',
         fidoVerifyAuthentication: 'Fido/verify-authentication/'
 
-    }
+    },
+		Issuance:{
+			getIssuedCredentials:'/issue-credentials'
+		}
 }
