5061b7096825b2aa6351703762c5d648e03d9a6b
bhavanakarwade <bhavana.karwade@ayanworks.com>
2023-08-08 13:36:35 +0530

    fix: resolved bug
    Signed-off-by: bhavanakarwade <bhavana.karwade@ayanworks.com>

--- src/components/Authentication/SignUpUser.tsx
+++ src/components/Authentication/SignUpUser.tsx
@@ -53,8 +53,8 @@ const SignUpUser = () => {
 
 
 	useEffect(() => {
-		
-		if(window?.location?.search.length > 7) {
+
+		if (window?.location?.search.length > 7) {
 			setEmailAutoFill(window?.location?.search.split('=')[1])
 		}
 	}, [])
