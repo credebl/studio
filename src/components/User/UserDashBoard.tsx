bb3982d808908eb2d85dc08fcede85c0aae7e7fc
@nishad.shirsat <nishad.shirsat@ayanworks.com>
2023-08-10 16:49:57 +0530

    Changed message of empty state for users activity
    Signed-off-by: @nishad.shirsat <nishad.shirsat@ayanworks.com>

--- src/components/User/UserDashBoard.tsx
+++ src/components/User/UserDashBoard.tsx
@@ -236,7 +236,7 @@ const UserDashBoard = () => {
 
 							</ol>
 							: activityList && (<EmptyListMessage
-								message={'No Organization'}
+								message={'No Users activity'}
 								description={'Get started by creating a new Organization'}
 								svgComponent={<svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="none" viewBox="0 0 24 24">
 									<path fill="#fff" d="M21.89 9.89h-7.78V2.11a2.11 2.11 0 1 0-4.22 0v7.78H2.11a2.11 2.11 0 1 0 0 4.22h7.78v7.78a2.11 2.11 0 1 0 4.22 0v-7.78h7.78a2.11 2.11 0 1 0 0-4.22Z" />
