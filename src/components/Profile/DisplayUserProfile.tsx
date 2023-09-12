10d7496b6439093e6ef09e0052cb3df34a429db5
bhavanakarwade <bhavana.karwade@ayanworks.com>
2023-09-12 11:19:11 +0530

    feat: display user profile
    Signed-off-by: bhavanakarwade <bhavana.karwade@ayanworks.com>

--- src/components/Profile/DisplayUserProfile.tsx
+++ src/components/Profile/DisplayUserProfile.tsx
@@ -1,17 +1,18 @@
 import type { UserProfile } from "./interfaces";
 import CustomAvatar from '../Avatar'
+import { Formik } from "formik";
 
 interface DisplayUserProfileProps {
     toggleEditProfile: () => void;
     userProfileInfo: UserProfile | null;
-  }
-  
-  const DisplayUserProfile = ({ toggleEditProfile, userProfileInfo }: DisplayUserProfileProps) => {
+}
+
+const DisplayUserProfile = ({ toggleEditProfile, userProfileInfo }: DisplayUserProfileProps) => {
 
     return (
-        <div className="max-w-lg mx-auto pl-2 pt-2 mb-4 bg-white border border-gray-200 rounded-lg shadow-sm 2xl:col-span-2 dark:border-gray-700 md:p-8 dark:bg-gray-800">
+        <div>
 
-            <div className="flow-root">
+            {/* <div className="flow-root">
                 <ul>
 
                     <li className="flex items-start justify-between pb-4">
@@ -77,7 +78,49 @@ interface DisplayUserProfileProps {
                         </div>
                     </li>
                 </ul>
+            </div> */}
+
+            <div className='h-full'>
+                <div className='page-container relative h-full flex flex-auto flex-col px-4 sm:px-6 md:px-8 py-4 sm:py-6 md:px-8'>
+                    <div className='container mx-auto bg-white'>
+                        <div className='card border-0 card-border'>
+                            <div className="px-6 py-6">
+                                <form action="#">
+                                    <div className="form-container">
+                                        <div>
+                                            <h1 className="text-black text-semibold text-xl text-opacity-70 font-montserrat">General</h1>
+                                            <p className="mt-2 text-gray-700 font-montserrat text-sm font-normal font-light leading-normal">Basic info, like your first name, last name and profile image that will be displayed</p>
+                                        </div>
+
+                                        <div className="grid md:grid-cols-3 gap-4 py-8 border-b border-gray-200 dark:border-gray-600 items-center">
+                                            <div className="text-base text-gray-700 font-montserrat">First Name</div>
+                                            <div className="focus:ring-indigo-600 col-span-2 w-full focus:ring-primary-500 focus:border-primary-500">
+                                                <input className="bg-gray-50 py-4 px-12 border border-gray-300 w-full rounded-md focus:ring-primary-500 focus:border-primary-500"/>
+                                            </div>
+                                        </div>
+
+                                        <div className="grid md:grid-cols-3 gap-4 py-8 border-b border-gray-200 dark:border-gray-600 items-center">
+                                            <div className="text-base text-gray-700 font-montserrat">Last Name</div>
+                                            <div className="focus:ring-indigo-600 col-span-2 w-full focus:ring-primary-500 focus:border-primary-500">
+                                                <input className="bg-gray-50 py-4 px-12 border border-gray-300 w-full rounded-md "/>
+                                            </div>
+                                        </div>
+
+                                        <div className="grid md:grid-cols-3 gap-4 py-8 border-b border-gray-200 dark:border-gray-600 items-center">
+                                            <div className="text-base text-gray-700 font-montserrat">Profile Image</div>
+                                            <div className="focus:ring-indigo-600 col-span-2 w-full focus-within:ring-indigo-600 focus-within:border-indigo-600 focus:border-indigo-600">
+                                                <input className="w-20 h-20 border border-gray-300 rounded-full"/>
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
+
         </div>
     );
 };
