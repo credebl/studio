cc00f946c1ced3b9a71d4e69a1949cf3f212fb14
pranalidhanavade <pranali.dhanavade@ayanworks.com>
2023-09-12 12:51:22 +0530

    feat:minor changes
    Signed-off-by: pranalidhanavade <pranali.dhanavade@ayanworks.com>

--- src/pages/subscription/index.tsx
+++ src/pages/subscription/index.tsx
@@ -9,8 +9,6 @@ import { MdDashboard } from 'react-icons/md';
 import React from 'react';
 import Subscription from './subscription';
 
-
-
 const initialPageState = {
     pageNumber: 1,
     pageSize: 10,
@@ -29,7 +27,7 @@ const Billing = () =>
             <div className="mb-4 border-b border-gray-200 dark:border-gray-700">
                 <ul className="pl-5 flex flex-wrap -mb-px text-sm font-medium text-center" id="myTab" data-tabs-toggle="#myTabContent" role="tablist">
                     <li className="mr-2" role="presentation">
-                        <button className="text-xl inline-block p-4 border-b-2 rounded-t-lg " id="profile-tab" data-tabs-target="#profile" type="button" role="tab" aria-controls="profile" aria-selected="false">OrgProfile</button>
+                        <button className="text-xl inline-block p-4 border-b-2 rounded-t-lg " id="profile-tab" data-tabs-target="#profile" type="button" role="tab" aria-controls="profile" aria-selected="false">Org Profile</button>
                     </li>
                     <li className="mr-2" role="presentation">
                         <button className="text-xl inline-block p-4 border-b-2 rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300" id="dashboard-tab" data-tabs-target="#dashboard" type="button" role="tab" aria-controls="dashboard" aria-selected="true">Subscription</button>
@@ -49,5 +47,4 @@ const Billing = () =>
         </div>
     )
 }
-
 export default Billing;
