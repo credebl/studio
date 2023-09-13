1fe6ac350bdd6cc0d1e32fb7e9bdf3d26573e78a
MoulikaKulkarni <moulika.kulkarni@ayanworks.com>
2023-09-13 17:51:30 +0530

    fix: date-tooltip
    Signed-off-by: MoulikaKulkarni <moulika.kulkarni@ayanworks.com>

--- src/commonComponents/DeviceDetailsCard.tsx
+++ src/commonComponents/DeviceDetailsCard.tsx
@@ -56,7 +56,7 @@ const DeviceDetails = (props: { deviceFriendlyName: string, createDateTime: stri
         }
         setTimeout(() => {
             props.refreshList()
-        }, 4000);
+        }, 6000);
  
     }
 
