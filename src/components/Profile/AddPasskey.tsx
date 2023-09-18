41a26c7fe44250f0f47a17bd66f1a423b7e678d9
bhavanakarwade <bhavana.karwade@ayanworks.com>
2023-09-18 11:41:50 +0530

    feat: worked on display user page
    Signed-off-by: bhavanakarwade <bhavana.karwade@ayanworks.com>

--- src/components/Profile/AddPasskey.tsx
+++ src/components/Profile/AddPasskey.tsx
@@ -13,6 +13,9 @@ import UpdatePassword from './UpdatePassword';
 import EditUserProfile from './EditUserProfile';
 import UpdateUserProfile from './EditUserProfile';
 import CustomSpinner from '../CustomSpinner';
+import React from 'react';
+import Invitations from '../organization/invitations/Invitations';
+import Members from '../organization/users/Members';
 
 const AddPasskey = () => {
   const [fidoError, setFidoError] = useState("")
@@ -205,32 +208,31 @@ const AddPasskey = () => {
           <div className=' h-full flex flex-auto flex-col justify-between'>
             <div className="mb-4 border-b border-gray-200 dark:border-gray-700">
               <ul className="pl-5 flex flex-wrap -mb-px text-sm font-medium text-center" id="myTab" data-tabs-toggle="#myTabContent" role="tablist">
-                <li className="mr-2" role="presentation">
-                  <button className="text-xl inline-block p-4 border-b-2 rounded-t-lg text-blue-600 hover:text-blue-600 dark:text-blue-500 dark:hover:text-blue-500 border-blue-600 dark:border-blue-500 " id="profile-tab" data-tabs-target="#profile" type="button" role="tab" aria-controls="profile" aria-selected="true">Profile</button>
-                </li>
-                <li className="mr-2" role="presentation">
-                  <button className="text-xl inline-block p-4 border-b-2 rounded-t-lg text-gray-500 hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300" id="password-tab" data-tabs-target="#dashboard" type="button" role="tab" aria-controls="dashboard" aria-selected="false">Password</button>
-                </li>
-                {/* <li className="mr-2" role="presentation">
-                  <button className="text-xl inline-block p-4 border-b-2 rounded-t-lg text-gray-500 hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300" id="dashboard-tab" data-tabs-target="#dashboard" type="button" role="tab" aria-controls="dashboard" aria-selected="false">Passkey</button>
-                </li> */}
+              <li className="mr-2" role="presentation">
+						<button className="text-xl inline-block p-4 border-b-2 rounded-t-lg text-blue-600 hover:text-blue-600 dark:text-blue-500 dark:hover:text-blue-500 border-blue-600 dark:border-blue-500" 
+                    id="profile-tab" data-tabs-target="#profile" type="button" role="tab" aria-controls="profile" aria-selected="false">Profile</button>
+					</li>
+					<li className="mr-2" role="presentation">
+						<button className="inline-block p-4 border-b-2 rounded-t-lg text-gray-500 hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 text-xl" id="dashboard-tab" data-tabs-target="#dashboard" type="button" role="tab" aria-controls="dashboard" aria-selected="false">Password</button>
+					</li>
 
               </ul>
             </div>
 
           </div>
-          <div id="myProfileTabContent">
-                <div className="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800" id="profile" role="tabpanel" aria-labelledby="profile-tab">
-                <DisplayUserProfile
-                  toggleEditProfile={toggleEditProfile}
-                  userProfileInfo={prePopulatedUserProfile}
-                />               
-                </div>
-                <div className="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800" id="password" role="tabpanel" aria-labelledby="password-tab">
-                    <UpdatePassword />
-                </div>
-            </div>
+          <div id="myTabContent">
+				<div className="rounded-lg bg-gray-50 dark:bg-gray-800" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+        <DisplayUserProfile
+            // toggleEditProfile={toggleEditProfile}
+            // userProfileInfo={prePopulatedUserProfile}
+          />
+				</div>
+				<div className="hidden rounded-lg bg-gray-50 dark:bg-gray-800" id="dashboard" role="tabpanel" aria-labelledby="dashboard-tab">
+					<UpdatePassword />
+				</div>
+			</div> 
         </div>
+
       }
     </div>
   );
