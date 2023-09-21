7701ab7e95055c7123c16ca8fc175c95d49bee5d
bhavanakarwade <bhavana.karwade@ayanworks.com>
2023-09-21 20:04:59 +0530

    feat: Developed display user profile page as per new designs
    Signed-off-by: bhavanakarwade <bhavana.karwade@ayanworks.com>

--- src/components/Profile/AddPasskey.tsx
+++ src/components/Profile/AddPasskey.tsx
@@ -1,50 +1,35 @@
-import { useEffect, useState } from 'react';
+import { Alert, Button } from 'flowbite-react'
+import React, { useEffect, useState } from 'react'
+import { IDeviceData, IdeviceBody, RegistrationOptionInterface } from './interfaces'
+import DeviceDetails from '../../commonComponents/DeviceDetailsCard'
+import PasskeyAddDevice from '../../commonComponents/PasseyAddDevicePopup'
+import { AxiosError, AxiosResponse } from 'axios'
+import { addDeviceDetails, generateRegistrationOption, getUserDeviceDetails, verifyRegistration } from '../../api/Fido'
+import { apiStatusCodes, storageKeys } from '../../config/CommonConstant'
+import { apiRoutes } from '../../config/apiRoutes'
 import { startRegistration } from '@simplewebauthn/browser'
-import type { AxiosError, AxiosResponse } from 'axios';
-import { addDeviceDetails, generateRegistrationOption, getUserDeviceDetails, verifyRegistration } from '../../api/Fido';
-import DeviceDetails from '../../commonComponents/DeviceDetailsCard';
-import { apiStatusCodes, storageKeys } from '../../config/CommonConstant';
-import { getFromLocalStorage, getUserProfile } from '../../api/Auth';
-import BreadCrumbs from '../BreadCrumbs';
-import { Alert } from 'flowbite-react';
-import type { IDeviceData, IdeviceBody, RegistrationOptionInterface, UserProfile, verifyRegistrationObjInterface } from './interfaces';
-import DisplayUserProfile from './DisplayUserProfile';
-import UpdatePassword from './UpdatePassword';
-import EditUserProfile from './EditUserProfile';
-import UpdateUserProfile from './EditUserProfile';
-import CustomSpinner from '../CustomSpinner';
-import React from 'react';
-import Invitations from '../organization/invitations/Invitations';
-import Members from '../organization/users/Members';
+import { getFromLocalStorage } from '../../api/Auth'
+import CustomSpinner from '../CustomSpinner'
 
 const AddPasskey = () => {
+
   const [fidoError, setFidoError] = useState("")
   const [fidoLoader, setFidoLoader] = useState(false)
   const [OrgUserEmail, setOrgUserEmail] = useState<string>('')
   const [deviceList, setDeviceList] = useState<IDeviceData[]>([])
   const [addSuccess, setAddSuccess] = useState<string | null>(null)
   const [addfailure, setAddFailur] = useState<string | null>(null)
-  const [isEditProfileOpen, setIsEditProfileOpen] = useState(false);
-  const [prePopulatedUserProfile, setPrePopulatedUserProfile] = useState<UserProfile | null>(null);
+  const [disableFlag, setDisableFlag] = useState<boolean>(false)
 
 
-  const fetchUserProfile = async () => {
-    try {
-      const token = await getFromLocalStorage(storageKeys.TOKEN);
-      const userDetailsResponse = await getUserProfile(token);
-      const { data } = userDetailsResponse as AxiosResponse;
+  const [openModel, setOpenModel] = useState<boolean>(false)
 
-      if (data?.statusCode === apiStatusCodes.API_STATUS_SUCCESS) {
-        setPrePopulatedUserProfile(data?.data);
-      }
-    } catch (error) {
-    }
-  };
-
-  const toggleEditProfile = async () => {
-    await fetchUserProfile()
-    setIsEditProfileOpen(!isEditProfileOpen);
-  };
+  const props = { openModel, setOpenModel };
+  const setProfile = async () => {
+    const UserEmail = await getFromLocalStorage(storageKeys.USER_EMAIL)
+    setOrgUserEmail(UserEmail)
+    return UserEmail
+  }
 
   const showFidoError = (error: unknown): void => {
     const err = error as AxiosError
@@ -70,17 +55,6 @@ const AddPasskey = () => {
       setFidoLoader(false)
     }
   }
-
-  const setProfile = async () => {
-    const UserEmail = await getFromLocalStorage(storageKeys.USER_EMAIL)
-    setOrgUserEmail(UserEmail)
-    return UserEmail
-  }
-
-  useEffect(() => {
-    fetchUserProfile();
-  }, []);
-
   const registerWithPasskey = async (flag: boolean): Promise<void> => {
     try {
       const RegistrationOption: RegistrationOptionInterface = {
@@ -165,22 +139,25 @@ const AddPasskey = () => {
       setFidoLoader(true)
 
       const userDeviceDetailsResp = await getUserDeviceDetails(OrgUserEmail)
+      const { data } = userDeviceDetailsResp as AxiosResponse
       setFidoLoader(false)
-
       if (userDeviceDetailsResp) {
-        const deviceDetails = Object.keys(userDeviceDetailsResp?.data?.data)?.length > 0 ?
+        const deviceDetails = Object.keys(data)?.length > 0 ?
           userDeviceDetailsResp?.data?.data.map((data) => {
             data.lastChangedDateTime = data.lastChangedDateTime ? data.lastChangedDateTime : "-"
             return data
           })
           : []
+        if (data?.data?.length === 1) {
+          setDisableFlag(true)
+        }
         setDeviceList(deviceDetails)
       }
     } catch (error) {
+      setAddFailur("Error while fetching the device details")
       setFidoLoader(false)
     }
   }
-
   useEffect(() => {
     if (OrgUserEmail) {
       userDeviceDetails();
@@ -189,16 +166,12 @@ const AddPasskey = () => {
     }
   }, [OrgUserEmail]);
 
-  const updatePrePopulatedUserProfile = (updatedProfile: UserProfile) => {
-    setPrePopulatedUserProfile(updatedProfile);
-  };
-
   return (
-
-    <div className="mb-4 col-span-full xl:mb-2 p-4">
-      <BreadCrumbs />
-
-      {/* {fidoLoader
+    <div className='h-full'>
+      <div className='page-container relative h-full flex flex-auto flex-col px-4 sm:px-6 md:px-8 py-4 sm:py-6 md:px-8'>
+        <div className='container mx-auto bg-white border border-gray-200 rounded-lg'>
+          <div className="px-6 py-6">
+                  {fidoLoader
         ? <div className="flex items-center justify-center mb-4">
 
           <CustomSpinner />
@@ -216,26 +189,68 @@ const AddPasskey = () => {
 						<button className="inline-block p-4 border-b-2 rounded-t-lg text-gray-500 hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 text-xl" id="dashboard-tab" data-tabs-target="#dashboard" type="button" role="tab" aria-controls="dashboard" aria-selected="false">Password</button>
 					</li>
 
-              </ul>
-            </div>
+            <form action="#">
+              <div className="form-container">
+                <div>
+                  <h1 className="text-gray-500 text-xl font-medium font-montserrat">Add Passkey</h1>
+                  <p className="mt-2 text-gray-700 font-montserrat text-sm font-normal font-light leading-normal">With Passkey, no complex passwords to remember.</p>
+                </div>
 
+                    {deviceList && deviceList.length > 0 &&
+                      deviceList.map((element, key) => (
+                        <DeviceDetails deviceFriendlyName={element['deviceFriendlyName']} createDateTime={element['createDateTime']} credentialID={element['credentialId']} refreshList={userDeviceDetails} disableRevoke={disableFlag} />
+                      ))}
+
+                    <div>
+
+                      <Button
+                        onClick={addDevice}
+                        type="submit"
+                        color='bg-primary-800'
+                        className='mt-10 text-base font-medium text-center text-white bg-primary-700 rounded-lg hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 sm:w-auto dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"'
+                      >
+                        <svg xmlns="http://www.w3.org/2000/svg" className="mr-2" width="18" height="18" fill="none" viewBox="0 0 18 18">
+                          <path stroke="#fff" d="M11.03 4.32a3.82 3.82 0 1 1-7.64 0 3.82 3.82 0 0 1 7.64 0Zm6.473 4.047a2.94 2.94 0 0 1-.486 1.62c-.315.476-.76.838-1.273 1.044l-.691.276.517.535.812.842-1.053 1.091-.335.348.335.347 1.053 1.091-1.619 1.678-.888-.92v-5.241l-.28-.138a2.774 2.774 0 0 1-1.098-.98 2.958 2.958 0 0 1-.168-2.917c.226-.455.566-.838.98-1.109a2.65 2.65 0 0 1 2.775-.081 2.79 2.79 0 0 1 1.038 1.05c.25.443.383.948.38 1.463Zm-1.55-1.761-.42.27.42-.27a1.434 1.434 0 0 0-.638-.542 1.396 1.396 0 0 0-1.566.32 1.491 1.491 0 0 0-.305 1.578c.105.265.286.494.52.656a1.403 1.403 0 0 0 1.813-.183 1.484 1.484 0 0 0 .175-1.83Zm-7.48 3.934c.664 0 1.32.122 1.934.359a5.18 5.18 0 0 0 1.332 1.626v4.213H.5v-1.3c0-1.291.537-2.535 1.5-3.456a5.284 5.284 0 0 1 3.649-1.443h2.824Z" />
+                        </svg>
+                        Add Passkey
+                      </Button>
+                      {
+                        (addSuccess || addfailure || fidoError) &&
+                        <div className='p-2'>
+                          <Alert
+                            color={addSuccess ? "success" : "failure"}
+                            onDismiss={() => {
+                              setAddSuccess(null)
+                              setFidoError('')
+                              setAddFailur('')
+                            }}
+                          >
+                            <span>
+                              <p>
+                                {addSuccess || addfailure || fidoError}
+                              </p>
+                            </span>
+                          </Alert>
+                        </div>
+                      }
+
+                </div>
+
+                <PasskeyAddDevice openModal={openModel} setOpenModel={props.setOpenModel} closeModal={function (flag: boolean): void {
+                  throw new Error('Function not implemented.');
+                }
+                }
+                  registerWithPasskey={registerWithPasskey}
+                />
+              </div>
+
+            </form> }
           </div>
-          <div id="myTabContent">
-				<div className="rounded-lg bg-gray-50 dark:bg-gray-800" id="profile" role="tabpanel" aria-labelledby="profile-tab">
-        <DisplayUserProfile
-            // toggleEditProfile={toggleEditProfile}
-            // userProfileInfo={prePopulatedUserProfile}
-          />
-				</div>
-				<div className="hidden rounded-lg bg-gray-50 dark:bg-gray-800" id="dashboard" role="tabpanel" aria-labelledby="dashboard-tab">
-					<UpdatePassword />
-				</div>
-			</div> 
         </div>
-
-      {/* } */}
+      </div>
     </div>
-  );
-};
 
-export default AddPasskey;
+  )
+}
+
+export default AddPasskey
--- src/components/Profile/UpdatePassword.tsx
+++ src/components/Profile/UpdatePassword.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const UpdatePassword = () => {
   return (
     <div className='h-full'>
-    <div className='page-container relative h-full flex flex-auto flex-col px-4 sm:px-6 md:px-8 py-4 sm:py-6 md:px-8'>
+    <div className='page-container relative h-full flex flex-auto flex-col px-4 sm:px-6 md:px-8 py-3 sm:py-6 md:px-8'>
         <div className='container mx-auto bg-white border border-gray-200 rounded-lg'>
                 <div className="px-6 py-6">
                     <form action="#">
@@ -17,21 +17,21 @@ const UpdatePassword = () => {
                             <div className="grid md:grid-cols-3 gap-4 py-8 border-b border-gray-200 dark:border-gray-600 items-center">
                                 <div className="text-base text-gray-700 font-montserrat">Current Password</div>
                                 <div className="focus:ring-indigo-600 col-span-2 w-full focus:ring-primary-500 focus:border-primary-500">
-                                    <input className="bg-gray-50 py-4 px-12 border border-gray-300 w-full rounded-md focus:ring-primary-500 focus:border-primary-500"/>
+                                    <input className="bg-gray-50 py-3 px-12 border border-gray-300 w-full rounded-md focus:ring-primary-500 focus:border-primary-500"/>
                                 </div>
                             </div>
 
                             <div className="grid md:grid-cols-3 gap-4 py-8 border-b border-gray-200 dark:border-gray-600 items-center">
                                 <div className="text-base text-gray-700 font-montserrat">New Password</div>
                                 <div className="focus:ring-indigo-600 col-span-2 w-full focus:ring-primary-500 focus:border-primary-500">
-                                    <input className="bg-gray-50 py-4 px-12 border border-gray-300 w-full rounded-md "/>
+                                    <input className="bg-gray-50 py-3 px-12 border border-gray-300 w-full rounded-md "/>
                                 </div>
                             </div>
 
                             <div className="grid md:grid-cols-3 gap-4 py-8 border-gray-200 dark:border-gray-600 items-center">
                                 <div className="text-base text-gray-700 font-montserrat">Confirm Password</div>
                                 <div className="focus:ring-indigo-600 col-span-2 w-full focus:ring-primary-500 focus:border-primary-500">
-                                    <input className="bg-gray-50 py-4 px-12 border border-gray-300 w-full rounded-md "/>
+                                    <input className="bg-gray-50 py-3 px-12 border border-gray-300 w-full rounded-md "/>
                                 </div>
                             </div>
                             <div className='float-right p-2'>
