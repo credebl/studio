04eef0eecae63215583f005ef38698f6976132ee
MoulikaKulkarni <moulika.kulkarni@ayanworks.com>
2023-09-21 13:11:14 +0530

    fix: dashboard UI changes
    Signed-off-by: MoulikaKulkarni <moulika.kulkarni@ayanworks.com>

--- src/components/User/UserDashBoard.tsx
+++ src/components/User/UserDashBoard.tsx
@@ -14,6 +14,7 @@ import { pathRoutes } from "../../config/pathRoutes";
 import { setToLocalStorage } from "../../api/Auth";
 import { dateConversion } from "../../utils/DateConversion";
 import DateTooltip from "../Tooltip";
+import React from "react";
 
 const initialPageState = {
 	pageNumber: 1,
@@ -144,7 +145,7 @@ const UserDashBoard = () => {
 					className=" justify-between p-4 bg-white border border-gray-200 rounded-lg shadow-sm sm:flex dark:border-gray-700 sm:p-6 dark:bg-gray-800"
 				>
 				<div className="w-full" >
-						<h2 className="text-base font-bold text-gray-500 dark:text-gray-400">
+						<h2 className="text-base font-bold text-gray-500 dark:text-gray-400 mb-2">
 							Organizations
 						</h2>
 
