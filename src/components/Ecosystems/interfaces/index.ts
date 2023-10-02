757e58212a4ba8c01c91309e0b72a2557975cbaa
pranalidhanavade <pranali.dhanavade@ayanworks.com>
2023-10-02 23:04:22 +0530

    feat:edit ecosystem modal
    Signed-off-by: pranalidhanavade <pranali.dhanavade@ayanworks.com>

---
+++ src/components/Ecosystems/interfaces/index.ts
@@ -0,0 +1,13 @@
+export interface Ecosystem {
+    id: number
+    createDateTime: string
+    createdBy: number
+    lastChangedDateTime: string
+    lastChangedBy: number
+    name: string
+    description: string
+    logoUrl: string
+    website: string
+    roles: string[]
+    
+}
