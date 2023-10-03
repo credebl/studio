7a48babfdc800fda8f94c9d2e3603b28375f6527
karan <karan.tompe@ayanworks.com>
2023-10-03 16:17:11 +0530

    removed unwanted code
    Signed-off-by: karan <karan.tompe@ayanworks.com>

--- src/components/Verification/SchemaCredDefDetails.tsx
+++ src/components/Verification/SchemaCredDefDetails.tsx
@@ -8,15 +8,15 @@ const SchemaCredDefDetails = ({ schemaCredDefList }: SchemaCredDefProps) => {
     return (
         <>
             {schemaCredDefList.map((item) => (
-                <div key={Object.values(item)[2]} className="flex justify-start ml-2 w-full mt-6 overflow-scroll">
+                <div key={Object.values(item)[2]} className="flex justify-start ml-2 w-full mt-6">
                     <div className="w-full">
                         <div className="flex flex-start mb-2 w-full ">
                             <div className=" w-3/12 font-semibold text-primary-700 dark:bg-gray-800 m-1 p-1 flex justify-start items-center">
                                 Schema Id
                             </div>
                             <div className=" flex items-center p-1 m-1 ">:</div>{' '}
-                            <div className="w-9/12 m-1 flex justify-start truncate text-gray-600 dark:text-white items-center cursor-pointer overflow-auto"
-                                 style={{overflow: 'auto'}}>
+                            <div className="w-9/12 m-1 flex justify-start text-gray-600 dark:text-white items-center cursor-pointer overflow-auto"
+                              >
                                 {Object.values(item)[2]}
                             </div>
                         </div>
@@ -29,7 +29,7 @@ const SchemaCredDefDetails = ({ schemaCredDefList }: SchemaCredDefProps) => {
                                     {' '}
                                     :
                                 </div>{' '}
-                                <div className="w-9/12 m-1 flex justify-start truncate text-gray-600 dark:text-white items-center overflow-auto">
+                                <div className="w-9/12 m-1 flex justify-start text-gray-600 dark:text-white items-center cursor-pointer overflow-auto">
                                     {Object.values(item)[1]
                                         ? Object.values(item)[1]
                                         : ''}
