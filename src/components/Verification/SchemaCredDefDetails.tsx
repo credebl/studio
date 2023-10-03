75e6eca9f65878bd26c404538bc4abb56580e34c
karan <karan.tompe@ayanworks.com>
2023-10-03 15:52:51 +0530

    added scroll and dark mode
    Signed-off-by: karan <karan.tompe@ayanworks.com>

--- src/components/Verification/SchemaCredDefDetails.tsx
+++ src/components/Verification/SchemaCredDefDetails.tsx
@@ -15,7 +15,7 @@ const SchemaCredDefDetails = ({ schemaCredDefList }: SchemaCredDefProps) => {
                                 Schema Id
                             </div>
                             <div className=" flex items-center p-1 m-1 ">:</div>{' '}
-                            <div className="w-9/12 m-1 flex justify-start truncate text-gray-600 dark:text-white items-center cursor-pointer overflow:hidden overflow:ellipsis"
+                            <div className="w-9/12 m-1 flex justify-start truncate text-gray-600 dark:text-white items-center cursor-pointer overflow-auto"
                                  style={{overflow: 'auto'}}>
                                 {Object.values(item)[2]}
                             </div>
@@ -29,7 +29,7 @@ const SchemaCredDefDetails = ({ schemaCredDefList }: SchemaCredDefProps) => {
                                     {' '}
                                     :
                                 </div>{' '}
-                                <div className="w-9/12 m-1 flex justify-start truncate text-gray-600 dark:text-white items-center overflow:hidden overflow:ellipsis">
+                                <div className="w-9/12 m-1 flex justify-start truncate text-gray-600 dark:text-white items-center overflow-auto">
                                     {Object.values(item)[1]
                                         ? Object.values(item)[1]
                                         : ''}
