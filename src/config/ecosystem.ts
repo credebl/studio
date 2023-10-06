def40b67b4149c5c1d5c993fcbafd3b5f4ac3130
sanjay-k1910 <sanjay.khatal@ayanworks.com>
2023-10-06 16:29:08 +0530

    refactor: added condition for roles
    Signed-off-by: sanjay-k1910 <sanjay.khatal@ayanworks.com>

--- src/config/ecosystem.ts
+++ src/config/ecosystem.ts
@@ -1,7 +1,7 @@
 import { EcosystemRoles } from "../common/enums"
 
 const isEnabledEcosystem = true
-const ecosystemRole = EcosystemRoles.ecosystemMember
+const ecosystemRole = EcosystemRoles.ecosystemLead
 
 const checkEcosystem = () => ({
     isEnabledEcosystem,
