2378f1e347d9efb999f21c265f4fd9c1997cb612
sanjay-k1910 <sanjay.khatal@ayanworks.com>
2023-10-11 21:38:23 +0530

    refactor: endorsement list
    Signed-off-by: sanjay-k1910 <sanjay.khatal@ayanworks.com>

--- src/components/Ecosystem/Endorsement/index.tsx
+++ src/components/Ecosystem/Endorsement/index.tsx
@@ -261,11 +261,11 @@ const EndorsementList = () => {
                                     </div>)
                                     :
                                     <div>
-                                        {walletStatus && !isEcosystemLead ?
+                                        {walletStatus ?
                                             <EmptyListMessage
                                                 message={'No Endorsement Requests'}
-                                                description={'Get started by requesting Endorsement'}
-                                                buttonContent={'Request Endorsements'}
+                                                description={''}
+                                                buttonContent={isEcosystemLead ? '' : 'Request Endorsements'}
                                                 svgComponent={<svg className='pr-2 mr-1' xmlns="http://www.w3.org/2000/svg" width="24" height="15" fill="none" viewBox="0 0 24 24">
                                                     <path fill="#fff" d="M21.89 9.89h-7.78V2.11a2.11 2.11 0 1 0-4.22 0v7.78H2.11a2.11 2.11 0 1 0 0 4.22h7.78v7.78a2.11 2.11 0 1 0 4.22 0v-7.78h7.78a2.11 2.11 0 1 0 0-4.22Z" />
                                                 </svg>}
