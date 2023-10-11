3217a8714f1e6891b2c1deea89097c8963fca999
karan <karan.tompe@ayanworks.com>
2023-10-11 20:14:20 +0530

    Merge branch 'fix: email field validation
    Signed-off-by: karan <karan.tompe@ayanworks.com>

--- src/components/organization/invitations/SendInvitationModal.tsx
+++ src/components/organization/invitations/SendInvitationModal.tsx
@@ -193,7 +193,6 @@ const SendInvitationModal = (props: {
 										id="email"
 										name="email"
 										className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-										type="email"
 									/>
 									{formikHandlers?.errors?.email &&
 									formikHandlers?.touched?.email ? (
