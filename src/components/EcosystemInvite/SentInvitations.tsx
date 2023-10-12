ca6b34a9ead576c655411d8b3ff1f006228b0ae8
karan <karan.tompe@ayanworks.com>
2023-10-12 17:47:31 +0530

    feat: ecosystem member list color code changes
    Signed-off-by: karan <karan.tompe@ayanworks.com>

--- src/components/EcosystemInvite/SentInvitations.tsx
+++ src/components/EcosystemInvite/SentInvitations.tsx
@@ -186,7 +186,7 @@ const SentInvitations = () => {
 															key={invitation.id}
 															className={`${
 																invitation.status === 'pending'
-																	? ' bg-blue-100 text-blue-800 dark:bg-blue-900 dark:text-blue-300'
+																	? 'bg-orange-100 text-orange-800 dark:bg-gray-700 dark:text-orange-300'
 																	: invitation.status === 'accepted'
 																	? 'bg-green-100 text-green-800 dark:bg-green-900 dark:text-green-300'
 																	: 'bg-red-100 text-red-800 dark:bg-red-900 dark:text-red-300'
