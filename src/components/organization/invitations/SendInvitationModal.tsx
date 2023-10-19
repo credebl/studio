78c7b87067d3bfb28e1b07540a207bdbbfc42529
karan <karan.tompe@ayanworks.com>
2023-10-19 15:55:59 +0530

    fix: send invitation field reset on action
    Signed-off-by: karan <karan.tompe@ayanworks.com>

--- src/components/organization/invitations/SendInvitationModal.tsx
+++ src/components/organization/invitations/SendInvitationModal.tsx
@@ -223,7 +223,7 @@ const SendInvitationModal = (props: {
 										<Label htmlFor="email2" value="Email" />
 										<span className="text-red-500 text-xs">*</span>
 									</div>
-									<input
+									<Field
 										id="email"
 										name="email"
 										className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
