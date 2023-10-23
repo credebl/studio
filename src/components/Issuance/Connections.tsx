c023fa5dc89bc9291d838d1e7f7570b8ab7a55e0
pranalidhanavade <pranali.dhanavade@ayanworks.com>
2023-10-23 19:41:17 +0530

    feat:bulk issuance
    Signed-off-by: pranalidhanavade <pranali.dhanavade@ayanworks.com>

--- src/components/Issuance/Connections.tsx
+++ src/components/Issuance/Connections.tsx
@@ -9,8 +9,8 @@ import { storageKeys } from "../../config/CommonConstant";
 import { pathRoutes } from "../../config/pathRoutes";
 import BreadCrumbs from "../BreadCrumbs";
 import ConnectionList from "./ConnectionList";
-import EmailList from "./EmailList";
 import BackButton from '../../commonComponents/backbutton'
+import BulkIssuance from "./BulkIssuance";
 
 
 const Connections = () => {
@@ -48,7 +48,7 @@ const Connections = () => {
 						<button className="inline-block p-4 border-b-2 rounded-t-lg text-xl" id="profile-tab" data-tabs-target="#profile" type="button" role="tab" aria-controls="profile" aria-selected="false">Connection</button>
 					</li>
 					<li className="mr-2" role="presentation">
-						<button className="inline-block p-4 border-b-2 rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 text-xl" id="dashboard-tab" data-tabs-target="#dashboard" type="button" role="tab" aria-controls="dashboard" aria-selected="false">Email</button>
+						<button className="inline-block p-4 border-b-2 rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 text-xl" id="dashboard-tab" data-tabs-target="#dashboard" type="button" role="tab" aria-controls="dashboard" aria-selected="false">Bulk Issuance</button>
 					</li>
 				</ul>
 			</div>
@@ -57,7 +57,7 @@ const Connections = () => {
 					<ConnectionList selectConnection={selectConnection} />
 				</div>
 				<div className="hidden rounded-lg bg-gray-50 dark:bg-gray-800" id="dashboard" role="tabpanel" aria-labelledby="dashboard-tab">
-					<EmailList />
+					<BulkIssuance />
 				</div>
 			</div>
 			<div className="flex items-center justify-between mb-4 pt-3">
