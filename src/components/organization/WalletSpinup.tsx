22f582d4fcc381d8bedc88aa9189177d5d0f5525
sanjay-k1910 <sanjay.khatal@ayanworks.com>
2023-10-26 13:50:20 +0530

    refactor: commented code
    Signed-off-by: sanjay-k1910 <sanjay.khatal@ayanworks.com>

--- src/components/organization/WalletSpinup.tsx
+++ src/components/organization/WalletSpinup.tsx
@@ -111,10 +111,6 @@ const SharedAgentForm = ({ orgName, seeds, isCopied, loading, copyTextVal, submi
 							name="label"
 							className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
 							type="text"
-						// onChange={(e) => {
-						// 	formikHandlers.handleChange(e)
-						// 	setOrganization(e.target.value)
-						// }}
 						/>
 						{formikHandlers?.errors?.label &&
 							formikHandlers?.touched?.label && (
