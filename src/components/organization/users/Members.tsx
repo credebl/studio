095e10a7a96bb96730f1a3699c2e90eabe36f836
Moulika Kulkarni <moulika.kulkarni@ayanworks.com>
2023-10-31 11:48:40 +0530

    feat:added approved icons
    Signed-off-by: Moulika Kulkarni <moulika.kulkarni@ayanworks.com>

--- src/components/organization/users/Members.tsx
+++ src/components/organization/users/Members.tsx
@@ -192,27 +192,27 @@ const Members = () => {
 												{!user.roles.includes(Roles.OWNER) &&
 												(userRoles.includes(Roles.OWNER) ||
 													userRoles.includes(Roles.ADMIN)) ? (
-													<p
+													<button
 														onClick={() => editUserRole(user)}
 														className="cursor-pointer mr-2 flex items-center text-sm font-medium text-gray-500 dark:text-gray-400"
 													>
 														<svg
 															className="h-5 w-6 mr-1 text-black dark:text-white"
 															viewBox="0 0 24 24"
-															stroke-width="2"
+															strokeWidth="2"
 															stroke="currentColor"
 															fill="none"
-															stroke-linecap="round"
-															stroke-linejoin="round"
+															strokeLinecap="round"
+															strokeLinejoin="round"
 														>
 															<path stroke="none" d="M0 0h24v24H0z" />
 															<path d="M9 7 h-3a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-3" />
 															<path d="M9 15h3l8.5 -8.5a1.5 1.5 0 0 0 -3 -3l-8.5 8.5v3" />
 															<line x1="16" y1="5" x2="19" y2="8" />
-														</svg>{' '}
-													</p>
+														</svg>{''}
+													</button>
 												) : (
-													<p
+													<button
 														onClick={() => editUserRole(user)}
 														className="invisible cursor-pointer mr-2 flex items-center text-sm font-medium text-gray-500 dark:text-gray-400"
 													>
@@ -225,12 +225,12 @@ const Members = () => {
 														>
 															<path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z"></path>
 															<path
-																fill-rule="evenodd"
+																fillRule="evenodd"
 																d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z"
 																clipRule="evenodd"
 															></path>
 														</svg>
-													</p>
+													</button>
 												)}
 											</div>
 										</li>
