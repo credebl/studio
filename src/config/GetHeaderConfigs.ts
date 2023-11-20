005edd7ffdc4f76a7d7978b3f80ac89d8330664d
pranalidhanavade <pranali.dhanavade@ayanworks.com>
2023-11-20 20:58:38 +0530

    feat:certificate image preview
    Signed-off-by: pranalidhanavade <pranali.dhanavade@ayanworks.com>

--- src/config/GetHeaderConfigs.ts
+++ src/config/GetHeaderConfigs.ts
@@ -1,14 +1,17 @@
-import { getFromLocalStorage } from "../api/Auth";
-import { storageKeys } from "./CommonConstant";
+import { getFromLocalStorage } from '../api/Auth';
+import { storageKeys } from './CommonConstant';
 
 export const getHeaderConfigs = async (tokenVal?: string) => {
-    const token = await getFromLocalStorage(storageKeys.TOKEN) || (typeof tokenVal === "string" ? tokenVal : "")
-
-    return {
-        headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${token}`
-        }
-    }
-
-}
+	const token =
+		(await getFromLocalStorage(storageKeys.TOKEN)) ||
+		(typeof tokenVal === 'string' ? tokenVal : '');
+	const config: any = {
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	};
+	if (token) {
+		config.headers['Authorization'] = `Bearer ${token}`;
+	}
+	return config;
+};
