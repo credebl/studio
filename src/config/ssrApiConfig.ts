0670dcd57365c01eccaf227c43580dd32844050a
pranalidhanavade <pranali.dhanavade@ayanworks.com>
2023-11-21 21:39:40 +0530

    feat:user certificate template
    Signed-off-by: pranalidhanavade <pranali.dhanavade@ayanworks.com>

--- src/config/ssrApiConfig.ts
+++ src/config/ssrApiConfig.ts
@@ -22,14 +22,11 @@ const API = async ({ token, url, method, payload }: IProps) => {
 			body: JSON.stringify(payload),
 		};
 		const baseURL = globalThis.baseUrl || envConfig.PUBLIC_BASE_URL || process.env.PUBLIC_BASE_URL;
-		console.log(77,baseURL);
 		const apiURL = baseURL + url;
 		const res = await fetch(apiURL, {
 			...config,
-		});
-		console.log(22,res);	
+		});	
 		const { data } = (await res.json()) || {};
-		console.log(33,data)
 		return data;
 	} catch (err) {
 		console.error('ERROR::', err);
