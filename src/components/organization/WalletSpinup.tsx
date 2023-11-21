85b052b5812e39f8f4fec6ce373fb998d07476f2
sanjay-k1910 <sanjay.khatal@ayanworks.com>
2023-11-21 22:50:00 +0530

    fix: sonar lint
    Signed-off-by: sanjay-k1910 <sanjay.khatal@ayanworks.com>

--- src/components/organization/WalletSpinup.tsx
+++ src/components/organization/WalletSpinup.tsx
@@ -76,7 +76,7 @@ const fetchNetworks = async () => {
 }
 
 const SharedAgentForm = ({ orgName, seeds, isCopied, loading, copyTextVal, submitSharedWallet }: ISharedAgentForm) => {
-	const [haveDid, setHaveDid] = useState(false)
+	const [haveDidShared, setHaveDidShared] = useState(false)
 	const [networks, setNetworks] = useState([])
 	const getLedgerList = async () => {
 		const res = await fetchNetworks()
@@ -97,18 +97,18 @@ const SharedAgentForm = ({ orgName, seeds, isCopied, loading, copyTextVal, submi
 			)
 			.min(2, 'Wallet label must be at least 2 characters')
 			.max(25, 'Wallet label must be at most 25 characters'),
-		seed: haveDid ? yup.string().required("Seed is required") : yup.string(),
-		did: haveDid ? yup.string().required("DID is required") : yup.string(),
+		seed: haveDidShared ? yup.string().required("Seed is required") : yup.string(),
+		did: haveDidShared ? yup.string().required("DID is required") : yup.string(),
 		network: yup.string().required("Network is required")
 	}
 
 	return (
 		<div className='mt-4 max-w-lg flex-col gap-4'>
 			<div className="flex items-center gap-2 mt-4">
-				<Checkbox id="haveDid" onChange={(e) => setHaveDid(e.target.checked)} />
+				<Checkbox id="haveDidShared" onChange={(e) => setHaveDidShared(e.target.checked)} />
 				<Label
 					className="flex"
-					htmlFor="haveDid"
+					htmlFor="haveDidShared"
 				>
 					<p>
 						Already have DID?
@@ -117,7 +117,7 @@ const SharedAgentForm = ({ orgName, seeds, isCopied, loading, copyTextVal, submi
 				</Label>
 			</div>
 			{
-				!haveDid &&
+				!haveDidShared &&
 				<div className='mt-4'>
 					<div className="block mb-1 text-sm font-medium text-gray-900 dark:text-white">
 						<Label value="Seed" />
@@ -132,8 +132,8 @@ const SharedAgentForm = ({ orgName, seeds, isCopied, loading, copyTextVal, submi
 								{`${isCopied}`} onClick={(e) => copyTextVal(e)}
 							>
 								{isCopied
-									? <svg className="h-6 w-6 text-white ml-2 text-base" width="25" height="25" viewBox="0 0 24 24" stroke-width="2" stroke="green" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z" />  <path d="M5 12l5 5l10 -10" /></svg>
-									: <svg className="h-6 w-6 text-green ml-2 text-base" width="25" height="25" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z" />  <rect x="8" y="8" width="12" height="12" rx="2" />  <path d="M16 8v-2a2 2 0 0 0 -2 -2h-8a2 2 0 0 0 -2 2v8a2 2 0 0 0 2 2h2" /></svg>
+									? <svg className="h-6 w-6 text-white ml-2 text-base" width="25" height="25" viewBox="0 0 24 24" strokeWidth={2} stroke="green" fill="none" strokeLinecap="round" strokeLinejoin="round">  <path stroke="none" d="M0 0h24v24H0z" />  <path d="M5 12l5 5l10 -10" /></svg>
+									: <svg className="h-6 w-6 text-green ml-2 text-base" width="25" height="25" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">  <path stroke="none" d="M0 0h24v24H0z" />  <rect x="8" y="8" width="12" height="12" rx="2" />  <path d="M16 8v-2a2 2 0 0 0 -2 -2h-8a2 2 0 0 0 -2 2v8a2 2 0 0 0 2 2h2" /></svg>
 								}
 							</button>
 						</span>
@@ -159,7 +159,7 @@ const SharedAgentForm = ({ orgName, seeds, isCopied, loading, copyTextVal, submi
 						onSubmit={formikHandlers.handleSubmit}
 					>
 						{
-							haveDid &&
+							haveDidShared &&
 							<>
 								<div className='mt-4'>
 									<div className="mb-1 block">
@@ -640,7 +640,7 @@ const WalletSpinup = (props: {
 												onChange={() => onRadioSelect(AgentType.SHARED)}
 												name="list-radio"
 												className="cursor-pointer w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500 mr-2"
-											/>
+/>
 											Shared
 										</label>
 									</div>
@@ -707,9 +707,9 @@ const WalletSteps = (props: { steps: number; agentSpinupCall: boolean }) => {
 							>
 								<path
 									stroke="currentColor"
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									stroke-width="2"
+									strokeLinecap="round"
+									strokeLinejoin="round"
+									strokeWidth={2}
 									d="M1 5.917 5.724 10.5 15 1.5"
 								/>
 							</svg>
@@ -752,9 +752,9 @@ const WalletSteps = (props: { steps: number; agentSpinupCall: boolean }) => {
 							>
 								<path
 									stroke="currentColor"
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									stroke-width="2"
+									strokeLinecap="round"
+									strokeLinejoin="round"
+									strokeWidth={2}
 									d="M1 5.917 5.724 10.5 15 1.5"
 								/>
 							</svg>
@@ -797,9 +797,9 @@ const WalletSteps = (props: { steps: number; agentSpinupCall: boolean }) => {
 							>
 								<path
 									stroke="currentColor"
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									stroke-width="2"
+									strokeLinecap="round"
+									strokeLinejoin="round"
+									strokeWidth={2}
 									d="M1 5.917 5.724 10.5 15 1.5"
 								/>
 							</svg>
@@ -842,9 +842,9 @@ const WalletSteps = (props: { steps: number; agentSpinupCall: boolean }) => {
 							>
 								<path
 									stroke="currentColor"
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									stroke-width="2"
+									strokeLinecap="round"
+									strokeLinejoin="round"
+									strokeWidth={2}
 									d="M1 5.917 5.724 10.5 15 1.5"
 								/>
 							</svg>
@@ -887,9 +887,9 @@ const WalletSteps = (props: { steps: number; agentSpinupCall: boolean }) => {
 							>
 								<path
 									stroke="currentColor"
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									stroke-width="2"
+									strokeLinecap="round"
+									strokeLinejoin="round"
+									strokeWidth={2}
 									d="M1 5.917 5.724 10.5 15 1.5"
 								/>
 							</svg>
