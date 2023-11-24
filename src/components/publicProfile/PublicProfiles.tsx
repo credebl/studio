e61a77de9e11e8fcf756d284183035e1c61148eb
sanjay-k1910 <sanjay.khatal@ayanworks.com>
2023-11-24 21:13:06 +0530

    refactor: explore organization page
    Signed-off-by: sanjay-k1910 <sanjay.khatal@ayanworks.com>

--- src/components/publicProfile/PublicProfiles.tsx
+++ src/components/publicProfile/PublicProfiles.tsx
@@ -9,10 +9,7 @@ const PublicProfile = () => {
 			<PublicNavbar limitedWidth={false} />
 			<div className="p-4 rounded-lg bg-gray-50 dark:bg-gray-800 shadow-sm">
 				<header></header>
-				<div className="pt-6">
-					<h1 className="ml-1 px-4 text-xl font-semibold text-gray-900 sm:text-2xl dark:text-white">
-						Organizations
-					</h1>
+				<div className="">
 					<div id="myTabContent">
 						<div
 							className="p-4 rounded-lg bg-gray-50 dark:bg-gray-800"
