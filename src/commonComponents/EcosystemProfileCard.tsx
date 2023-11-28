eaa76e022872b16a99cabadb73b05ead065a35e5
karan <karan.tompe@ayanworks.com>
2023-11-28 16:17:24 +0530

    fix: go to dashboard svg
    Signed-off-by: karan <karan.tompe@ayanworks.com>

--- src/commonComponents/EcosystemProfileCard.tsx
+++ src/commonComponents/EcosystemProfileCard.tsx
@@ -116,11 +116,8 @@ const EcosystemProfileCard = () => {
                                 }
                             </select>
                         }
-                        <a href={pathRoutes.ecosystem.dashboard} className='flex text-primary-700 dark:text-secondary-700 text-sm font-medium underline-offset-4 hover:underline'>
+                        <a href={pathRoutes?.ecosystem?.dashboard} className='flex text-primary-700 dark:text-secondary-700 text-sm font-medium underline-offset-4 hover:underline'>
                             Go to Dashboard
-                            <svg fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" className='h-4 w-4 text-primary-700 dark:text-secondary-700 ml-1'>
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"></path>
-                            </svg>
                         </a>
                     </div>
 
