727db209fbd4fe298a94f1bdcf8858b30165bc05
bhavanakarwade <bhavana.karwade@ayanworks.com>
2023-11-29 11:39:39 +0530

    fix: applied validations on input boxes
    Signed-off-by: bhavanakarwade <bhavana.karwade@ayanworks.com>

--- src/components/organization/EditOrgdetailsModal.tsx
+++ src/components/organization/EditOrgdetailsModal.tsx
@@ -316,17 +316,13 @@ const EditOrgdetailsModal = (props: EditOrgdetailsModalProps) => {
                                         formikHandlers.setFieldTouched(
                                             'name',
                                             true,
+                                            false
                                         );
 
                                         if (value.length > 50) {
                                             formikHandlers.setFieldError(
                                                 'name',
                                                 'Organization name must be at most 50 characters',
-                                            );
-                                        } else {
-                                            formikHandlers.setFieldError(
-                                                'name',
-                                                undefined,
                                             );
                                         }
                                     }}
