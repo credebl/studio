9655c6123caf840fad77f7e4c204084cb0a22505
karan <karan.tompe@ayanworks.com>
2023-12-05 13:28:07 +0530

    handled paths and routes
    Signed-off-by: karan <karan.tompe@ayanworks.com>

--- src/components/Issuance/IssueDashboard.tsx
+++ src/components/Issuance/IssueDashboard.tsx
@@ -9,23 +9,21 @@ const IssueDashboard = () => {
 			heading: 'Connection',
 			description:
 				'Issue credential(s) by selecting connection from existing users',
-				path: pathRoutes.organizations.Issuance.schema
-
+			path: pathRoutes.organizations.Issuance.schema,
 		},
 		{
 			heading: 'Email',
 			description: 'Issue credential(s) by entering email ID for specific user',
-			path: pathRoutes.organizations.Issuance.email
-
+			path: pathRoutes.organizations.Issuance.email,
 		},
 		{
 			heading: 'Bulk',
 			description: 'Issue credential(s) by uploading .csv file records',
-			path: pathRoutes.organizations.Issuance.bulkIssuance
+			path: pathRoutes.organizations.Issuance.bulkIssuance,
 		},
 	];
 	return (
-		<div className="h-[700px] mx-2">
+		<div className="md:h-[750px] h-screen mx-2">
 			<div className="mb-2 col-span-full xl:mb-2">
 				<BreadCrumbs />
 			</div>
@@ -34,69 +32,41 @@ const IssueDashboard = () => {
 					Issue Credential
 				</h1>
 			</div>
-		<div className='bg-white text-lg shadow-md dark:border-gray-700 dark:bg-gray-800 h-full items-center w-full'>
-			<p className='pl-4 pt-6'>Choose following action for issue credential(s)</p>
-			<div className="flex justify-center bg-white shadow-md dark:border-gray-700 dark:bg-gray-800 h-full items-center w-full">
-				<div className="flex justify-center h-full items-center w-10/12 grid grid-cols-1 xl:grid-cols-2 2xl:grid-cols-3 dark:border-gray-700 dark:bg-gray-800 px-auto">
-					{options.map((option) => {
-						return (
-							<Card
-								className="transform transition duration-500 hover:scale-105 hover:bg-gray-50 cursor-pointer overflow-hidden overflow-ellipsis"
-								style={{
-									maxHeight: '100%',
-									maxWidth: '90%',
-									overflow: 'auto',
-									height: '168px',
-								}}
-								onClick={()=>window.location.href=option?.path}
-							>
-								<div className="flex items-center min-[401px]:flex-nowrap flex-wrap">
-									<div className="ml-4 w-100/6rem line-clamp-4 ">
-										<h5 className="text-2xl font-semibold tracking-tight text-primary-700 dark:text-white">
-											{option.heading}
-										</h5>
-										<p className="text-sm tracking-tight text-gray-700 dark:text-white">
-											{option.description}
-										</p>
+			<div className="bg-white text-lg shadow-md dark:border-gray-700 dark:bg-gray-800 h-full items-center w-full">
+				<p className="pl-4 pt-6 text-gray-900 text-lg dark:text-white">
+					Choose following action for issue credential(s)
+				</p>
+				<div className="flex justify-center bg-white shadow-md dark:border-gray-700 dark:bg-gray-800 h-full items-center w-full">
+					<div className="flex justify-center h-full items-center w-10/12 grid grid-cols-1 xl:grid-cols-2 2xl:grid-cols-3 dark:border-gray-700 dark:bg-gray-800 px-auto">
+						{options.map((option) => {
+							return (
+								<Card
+									key={option.heading}
+									className="group transform transition duration-500 hover:scale-105 cursor-pointer overflow-hidden overflow-ellipsis hover:bg-primary-700 hover:text-white"
+									style={{
+										maxHeight: '100%',
+										maxWidth: '90%',
+										overflow: 'auto',
+										height: '168px',
+									}}
+									onClick={() => (window.location.href = option?.path)}
+								>
+									<div className="flex items-center min-[401px]:flex-nowrap flex-wrap">
+										<div className="ml-4">
+											<h5 className="text-2xl font-semibold text-primary-700 dark:text-white pb-2">
+												{option.heading}
+											</h5>
+											<p className="text-sm tracking-tight text-gray-700 dark:text-white">
+												{option.description}
+											</p>
+										</div>
 									</div>
-								</div>
-							</Card>
-						);
-					})}
-					{/* <Card key={org.id} onClick={() => redirectOrgDashboard(org)} className='transform transition duration-500 hover:scale-105 hover:bg-gray-50 cursor-pointer overflow-hidden overflow-ellipsis' style={{ maxHeight: '100%', maxWidth: '100%', overflow: 'auto' }}> */}
-
-					{/* <Card
-						className="transform transition duration-500 hover:scale-105 hover:bg-gray-50 cursor-pointer overflow-hidden overflow-ellipsis"
-						style={{ maxHeight: '100%', maxWidth: '90%', overflow: 'auto' , height: '168px'}}
-					>
-						<div className="flex items-center min-[401px]:flex-nowrap flex-wrap">
-							<div className="ml-4 w-100/6rem line-clamp-4 ">
-								<h5 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
-									name
-								</h5>
-								<p className="text-base tracking-tight text-gray-900 dark:text-white truncate">
-									description
-								</p>
-							</div>
-						</div>
-					</Card>
-					<Card
-						className="transform transition duration-500 hover:scale-105 hover:bg-gray-50 cursor-pointer overflow-hidden overflow-ellipsis"
-						style={{ maxHeight: '100%', maxWidth: '90%', overflow: 'auto' , height: '168px'}}
-					>
-						<div className="flex items-center min-[401px]:flex-nowrap flex-wrap">
-							<div className="ml-4 w-100/6rem line-clamp-4 ">
-								<h5 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
-									name
-								</h5>
-								<p className="text-base tracking-tight text-gray-900 dark:text-white truncate">
-									description
-								</p>
-							</div>
-						</div>
-					</Card> */}
+								</Card>
+							);
+						})}
+					</div>
 				</div>
-			</div></div>
+			</div>
 		</div>
 	);
 };
