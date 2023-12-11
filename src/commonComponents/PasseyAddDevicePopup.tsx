b1b1f9184253a6e758307832c984cc9c2553ea2c
sanjay-k1910 <sanjay.khatal@ayanworks.com>
2023-12-11 16:01:30 +0530

    fix: show password icon alignment
    Signed-off-by: sanjay-k1910 <sanjay.khatal@ayanworks.com>

--- src/commonComponents/PasseyAddDevicePopup.tsx
+++ src/commonComponents/PasseyAddDevicePopup.tsx
@@ -114,7 +114,7 @@ const PasskeyAddDevice = (props: {
 										<Field
 											id="signinPassword"
 											name="Password"
-											className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+											className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 !pr-10 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
 											type={passwordVisible ? 'text' : 'password'}
 										/>
 										<button
