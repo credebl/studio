d887d31ad86f9841b86ed28ac64f3e27df6c64fa
karan <karan.tompe@ayanworks.com>
2024-01-24 19:47:29 +0530

    handled role wise access
    Signed-off-by: karan <karan.tompe@ayanworks.com>

--- src/common/enums.ts
+++ src/common/enums.ts
@@ -61,7 +61,8 @@ export enum OrganizationRoles {
 	organizationMember = "member",
 	organizationOwner = "owner",
 	organizationIssuer = "issuer",
-	organizationVerifier = "verifier"
+	organizationVerifier = "verifier",
+	organizationAdmin= 'admin'
 }
 
 export enum PlatformRoles {
