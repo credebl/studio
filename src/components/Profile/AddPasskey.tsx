56f6daa0fb6f411bcb29a1fd0c83dda85588a19c
karan <karan.tompe@ayanworks.com>
2024-02-05 13:30:46 +0530

    extra space between words

--- src/components/Profile/AddPasskey.tsx
+++ src/components/Profile/AddPasskey.tsx
@@ -1,5 +1,5 @@
 import { Alert, Button } from 'flowbite-react';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import DeviceDetails from '../../commonComponents/DeviceDetailsCard';
 import PasskeyAddDevice from '../../commonComponents/PasseyAddDevicePopup';
 import {
@@ -223,7 +223,7 @@ const AddPasskey = ({ responseMessages }: { responseMessages: (value: IResponseM
 										<h1 className="text-gray-500 text-xl font-medium font-montserrat dark:text-white">
 											Add Passkey
 										</h1>
-										<p className="mt-2 text-gray-700 font-montserrat text-sm font-normal font-light leading-normal dark:text-white">
+										<p className="text-start mt-2 text-gray-700 font-montserrat text-sm font-normal font-light leading-normal dark:text-white">
 											With Passkey, no complex passwords to remember.
 										</p>
 									</div>
--- src/components/Profile/EditUserProfile.tsx
+++ src/components/Profile/EditUserProfile.tsx
@@ -241,7 +241,7 @@ const EditUserProfile = ({ toggleEditProfile, userProfileInfo, updateProfile }: 
 
                         <div>
                           <h1 className="text-gray-500 text-xl font-medium font-montserrat dark:text-white">General</h1>
-                          <p className="mt-2 text-gray-700 font-montserrat text-sm font-normal font-light leading-normal dark:text-white">Basic info, like your first name, last name and profile image that will be displayed</p>
+                          <p className="text-start mt-2 text-gray-700 font-montserrat text-sm font-normal font-light leading-normal dark:text-white">Basic info, like your first name, last name and profile image that will be displayed</p>
                         </div>
 
                       </div>
