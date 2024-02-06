8debe3c88c13bfe17dab19f62a131dd70d5231cb
karan <karan.tompe@ayanworks.com>
2024-02-06 13:13:14 +0530

    role check added
    Signed-off-by: karan <karan.tompe@ayanworks.com>

--- src/utils/check-session.ts
+++ src/utils/check-session.ts
@@ -6,6 +6,7 @@ import { RolePermissions } from '../config/permissions';
 
 interface IProps {
 	cookies: AstroCookies;
+	currentPath: string;
 }
 
 interface IOutput {
@@ -16,6 +17,7 @@ interface IOutput {
 
 export const checkUserSession = async ({
 	cookies,
+	currentPath
 }: IProps): Promise<IOutput> => {
 	const sessionCookie = getFromCookies(cookies, 'session');
 
@@ -26,7 +28,26 @@ export const checkUserSession = async ({
 			authorized: false,
 		};
 	}
+	const role = getFromCookies(cookies, 'role');
+	const permittedPages = RolePermissions.find(
+		(item) => item.role === role,
+	)?.pages;
 
+	if (!permittedPages?.includes(currentPath)) {
+		if (permittedPages) {
+			return {
+				permitted: false,
+				redirect: permittedPages[0],
+				authorized: true,
+			};
+		} else {
+			return {
+				permitted: true,
+				redirect: pathRoutes.users.dashboard,
+				authorized: true,
+			};
+		}
+	}
 	return {
 		permitted: true,
 		authorized: true,
