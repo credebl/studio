5fee5a9d841658eaef43c7367b9d2d38b0b673fc
karan <karan.tompe@ayanworks.com>
2024-02-07 18:59:33 +0530

    feat: forgot password
    Signed-off-by: karan <karan.tompe@ayanworks.com>

--- src/config/apiRoutes.ts
+++ src/config/apiRoutes.ts
@@ -14,7 +14,9 @@ export const apiRoutes = {
 		fidoDevice: 'auth/passkey',
 		fidoAuthentication: 'auth/passkey/authentication-options',
 		fidoVerifyAuthentication: 'auth/passkey/verify-authentication/',
-		keyClockResetPassword: 'auth/reset-password'
+		keyClockResetPassword: 'auth/reset-password',
+		forgotPassword: 'auth/forgot-password',
+		resetPassword: 'auth/password-reset'
 	},
 	users: {
 		userProfile: '/users/profile',
