302be7fc397ebac487df8b2f71d05ad7fb8ca8be
karan <karan.tompe@ayanworks.com>
2024-02-08 20:55:29 +0530

    fix: added width
    Signed-off-by: karan <karan.tompe@ayanworks.com>

--- src/components/Ecosystem/Dashboard.tsx
+++ src/components/Ecosystem/Dashboard.tsx
@@ -301,7 +301,7 @@ const Dashboard = () => {
 											<CustomAvatar size="90" name={ecosystemDetails?.name} />
 										)}
 									</div>
-									<div>
+									<div className='w-full'>
 										<div className="w-full">
 											<h3 className="mb-1 text-xl font-bold text-gray-900 dark:text-white">
 												{ecosystemDetails?.name}
