762712456a3ceb0455415e646bb1a3ca9b2e18c8
karan <karan.tompe@ayanworks.com>
2024-02-08 19:35:43 +0530

    fix: issues from live tracker
    Signed-off-by: karan <karan.tompe@ayanworks.com>

--- src/components/organization/EditOrgdetailsModal.tsx
+++ src/components/organization/EditOrgdetailsModal.tsx
@@ -262,10 +262,10 @@ const EditOrgdetailsModal = (props: EditOrgdetailsModalProps) => {
 									as="textarea"
 									className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
 									placeholder="Description of your organization"
-									onChange={(e) => {
+									onChange={(e: { target: { value: string; }; }) => {
 										const value = e.target.value;
 										formikHandlers.setFieldValue('description', value);
-										formikHandlers.setFieldTouched('description', true);
+										formikHandlers.setFieldTouched('description', true, false);
 
 										if (value.length > 50) {
 											formikHandlers.setFieldError(
