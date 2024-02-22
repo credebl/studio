602fa5e6a03aa7a10b241213555edd30cadaae8a
karan <karan.tompe@ayanworks.com>
2024-02-22 13:55:16 +0530

    resolved merge conflicts
    Signed-off-by: karan <karan.tompe@ayanworks.com>

--- src/components/Issuance/Issuance.tsx
+++ src/components/Issuance/Issuance.tsx
@@ -198,24 +198,127 @@ const IssueCred = () => {
 						? issuanceFormPayload.map((user) => (
 							<Formik
 								key={user.connectionId}
-								initialValues={user}
-								validationSchema={validationSchema}
-								onSubmit={handleSubmit}
-							>
-								{({ values }) => (
-									<Form>
-										<Card
-											className="my-5"
-											style={{
-												maxWidth: '100%',
-												maxHeight: '100%',
-												overflow: 'auto',
-											}}
-										>
-											<div className="flex justify-between">
-												<h5 className="text-xl font-bold leading-none dark:text-white">
-													{user.userName}
-												</h5>
+									initialValues={user}
+									validationSchema={validationSchema}
+									onSubmit={handleSubmit}
+								>
+									{({ values, errors, touched, isValid }) => (
+										<Form>
+											<Card
+												className="my-5"
+												style={{
+													maxWidth: '100%',
+													maxHeight: '100%',
+													overflow: 'auto',
+												}}
+											>
+												<div className="flex justify-between">
+													<h5 className="text-xl font-bold leading-none dark:text-white">
+														{user.userName}
+													</h5>
+												</div>
+												<div className="flex">
+													<h5 className="text-xl font-bold leading-none dark:text-white flex flex-wrap">
+														Connection Id
+													</h5>
+													<span className="text-xl font-bold leading-none dark:text-white pl-1">
+														:
+													</span>
+													<p className="dark:text-white pl-1">
+														{user?.connectionId}
+													</p>
+												</div>
+												<h3 className="dark:text-white">Attributes</h3>
+												<div className="container mx-auto pr-2">
+													<div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-2">
+														{schemaAttributesDetails &&
+															schemaAttributesDetails?.length > 0 &&
+															schemaAttributesDetails?.map((attr, index) => (
+																<div key={attr.attributeName}>
+																	<div
+																		key={attr?.attributeName}
+																		className="flex"
+																	>
+																		<label
+																			htmlFor={`attributes.${index}.value`}
+																			className="dark:text-white w-2/5 pr-3 flex justify-end items-center font-light"
+																		>
+																			<div className="flex items-center word-break-word text-end">
+																				{' '}
+																				{attr?.displayName
+																					.split("_").map(item => item[0].toUpperCase() + item.slice(1,  item.length)).join(" ")}
+																				{attr?.isRequired && (
+																					<span className="text-red-500">
+																						*
+																					</span>
+																				)} :
+																			</div>
+																		</label>
+																		<Field
+																			type={
+																				attr?.schemaDataType === 'date'
+																					? 'date'
+																					: attr?.schemaDataType}
+																			id={`attributes.${index}.value`}
+																			name={`attributes.${index}.value`}
+																			className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 w-3/5"
+																		/>
+																	</div>
+																	<div className="flex">
+																		<div className="w-1/4 pr-2"></div>
+																		{errors?.attributes &&
+																			errors?.attributes[index] &&
+																			touched?.attributes[index] &&
+																			errors?.attributes[index]?.value && (
+																				<div className="text-red-500 text-xs w-3/4 p-1">
+																					{errors?.attributes[index]?.value}
+																				</div>
+																			)}
+																	</div>
+																</div>
+															))}
+													</div>
+												</div>
+											</Card>
+											{failure && (
+												<div className="pt-1 pb-1">
+													<Alert
+														color="failure"
+														onDismiss={() => setFailure(null)}
+													>
+														<span>
+															<p>{failure}</p>
+														</span>
+													</Alert>
+												</div>
+											)}
+											<div className="flex justify-end">
+												<Button
+													type="submit"
+													disabled={!isValid || issuanceLoader}
+													isProcessing={issuanceLoader}
+													className='text-base text-center text-white bg-primary-700 hover:!bg-primary-800 rounded-lg hover:bg-accent-00 focus:ring-4 focus:ring-primary-300 sm:w-auto dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"'
+												>
+													<div className="pr-3">
+														<svg
+															xmlns="http://www.w3.org/2000/svg"
+															width="24"
+															height="24"
+															fill="none"
+															viewBox="0 0 24 24"
+														>
+															<path
+																fill="#fff"
+																d="M12.516 6.444a.556.556 0 1 0-.787.787l4.214 4.214H4.746a.558.558 0 0 0 0 1.117h11.191l-4.214 4.214a.556.556 0 0 0 .396.95.582.582 0 0 0 .397-.163l5.163-5.163a.553.553 0 0 0 .162-.396.576.576 0 0 0-.162-.396l-5.163-5.164Z"
+															/>
+															<path
+																fill="#fff"
+																d="M12.001 0a12 12 0 0 0-8.484 20.485c4.686 4.687 12.283 4.687 16.969 0 4.686-4.685 4.686-12.282 0-16.968A11.925 11.925 0 0 0 12.001 0Zm0 22.886c-6 0-10.884-4.884-10.884-10.885C1.117 6.001 6 1.116 12 1.116s10.885 4.885 10.885 10.885S18.001 22.886 12 22.886Z"
+															/>
+														</svg>
+													</div>
+													Issue
+												</Button>
 											</div>
 											<div className="flex">
 												<h5 className="text-xl font-bold leading-none dark:text-white flex flex-wrap">
