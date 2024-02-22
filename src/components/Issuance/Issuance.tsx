9dfdf2f0e414ef5f4aed0bf1baaccca3c1ceeba6
karan <karan.tompe@ayanworks.com>
2024-02-22 13:55:16 +0530

    resolved merge conflicts
    Signed-off-by: karan <karan.tompe@ayanworks.com>

--- src/components/Issuance/Issuance.tsx
+++ src/components/Issuance/Issuance.tsx
@@ -265,8 +265,7 @@ const IssueCred = () => {
 																			type={
 																				attr?.schemaDataType === 'date'
 																					? 'date'
-																					: attr?.schemaDataType
-																			}
+																					: attr?.schemaDataType}
 																			id={`attributes.${index}.value`}
 																			name={`attributes.${index}.value`}
 																			className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 w-3/5"
