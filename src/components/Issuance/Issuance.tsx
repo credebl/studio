199513a7584e54a635b8986201a4f280bed2a59e
karan <karan.tompe@ayanworks.com>
2024-02-23 22:51:54 +0530

    fix: issuance page break
    Signed-off-by: karan <karan.tompe@ayanworks.com>

--- src/components/Issuance/Issuance.tsx
+++ src/components/Issuance/Issuance.tsx
@@ -270,15 +270,16 @@ const IssueCred = () => {
 																			className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 w-3/5"
 																		/>
 																		<div className="absolute bottom-[-20px] right-0">
-																		{errors?.attributes &&
-																			errors?.attributes[index] &&
-																			touched?.attributes[index] &&
-																			errors?.attributes[index]?.value && (
-																				<div className="text-red-500 text-xs p-1">
-																					{errors?.attributes[index]?.value}
-																				</div>
-																			)}
-																	</div>
+																			{errors?.attributes &&
+																				errors?.attributes[index] &&
+																				touched?.attributes &&
+																				touched?.attributes[index] &&
+																				errors?.attributes[index]?.value && (
+																					<div className="text-red-500 text-xs p-1">
+																						{errors?.attributes[index]?.value}
+																					</div>
+																				)}
+																		</div>
 																	</div>
 																</div>
 															))}
