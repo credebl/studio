b66ebcd1cb2e3e10c4670531d8a192b6d21b4d54
karan <karan.tompe@ayanworks.com>
2024-02-29 18:25:00 +0530

    fix: user role access
    Signed-off-by: karan <karan.tompe@ayanworks.com>

--- src/components/organization/invitations/SendInvitationModal.tsx
+++ src/components/organization/invitations/SendInvitationModal.tsx
@@ -323,7 +323,7 @@ const SendInvitationModal = (props: {
 					<div className="mt-4 flex justify-end">
 						<Button
 							onClick={props.flag ? sendEcoSystemInvitations : sendInvitations}
-							disabled={invitations.length === 0}
+							disabled={loading || invitations.length === 0}
 							isProcessing={loading}
 							className='text-base font-medium text-center text-white bg-primary-700 hover:!bg-primary-800 rounded-lg hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 sm:w-auto dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"'
 						>
