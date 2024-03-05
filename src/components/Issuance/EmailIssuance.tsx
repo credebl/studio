6b11c2ac70dd63f0274a830d79f18cb9457ceeb2
Nishad <nishad.shirsat@ayanworks.com>
2024-03-05 19:58:06 +0530

    fixed the email bulk issuance response
    Signed-off-by: Nishad <nishad.shirsat@ayanworks.com>

--- src/components/Issuance/EmailIssuance.tsx
+++ src/components/Issuance/EmailIssuance.tsx
@@ -115,7 +115,7 @@ const EmailIssuance = () => {
 			transformedData.credentialDefinitionId = credentialSelected;
 			const transformedJson = JSON.stringify(transformedData, null, 2);
 			const response = await issueOobEmailCredential(transformedJson);
-			const { data } = response as AxiosResponse;
+			const { data } = response as AxiosResponse;			
 
 			if (data?.statusCode === apiStatusCodes.API_STATUS_CREATED) {
 				if (data?.data) {
@@ -130,9 +130,10 @@ const EmailIssuance = () => {
 					setTimeout(() => {
 						window.location.href = pathRoutes?.organizations?.issuedCredentials;
 					}, 500);
-				} else {
-					setFailure(response as string);
+				} else {					
+					setFailure(data?.message);
 					setLoading(false);
+					setIssueLoader(false);
 					setOpenModal(false);
 				}
 			} else {
