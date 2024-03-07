e6d450d8ad66c4df4d30a608eb3a14a9d776a858
bhavanakarwade <bhavana.karwade@ayanworks.com>
2024-03-07 18:33:32 +0530

    feat: support multiple did methods at the time of wallet creation
    Signed-off-by: bhavanakarwade <bhavana.karwade@ayanworks.com>

--- src/components/organization/WalletSpinup.tsx
+++ src/components/organization/WalletSpinup.tsx
@@ -422,24 +422,6 @@ const SharedAgentForm = ({
 									</>
 								)}
 
-								{formikHandlers.values.method === DidMethod.POLYGON && (
-									<>
-										<div className="my-3 relative">
-											<div className="mt-4">
-												<Label value="Enter Endpoint" />
-												<span className="text-red-500 text-xs">*</span>
-											</div>
-										</div>
-										<Field
-											id="endpoint"
-											name="endpoint"
-											className="truncate bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 h-11"
-											value={endPointValue}
-											onChange={(e) => setEndPointValue(e.target.value)}
-											placeholder="Please enter endpoint"
-										/>
-									</>
-								)}
 
 								{formikHandlers.values.method === DidMethod.WEB && (
 									<>
@@ -966,7 +948,6 @@ const WalletSpinup = (props: {
 			ledger: values.method === DidMethod.INDY ? values.ledger : '',
 			label: values.label,
 			privatekey: values.method === DidMethod.POLYGON ? polygonPrivateKey : '',
-			endpoint: values.method === DidMethod.POLYGON ? endPoint : '',
 			seed: values.method === DidMethod.POLYGON ? '' : values.seed || seeds,
 			network:
 				values.method === DidMethod.POLYGON
