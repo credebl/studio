cc1319e0ae5a137d6f4fbc140a406294ebb12e15
pranalidhanavade <pranali.dhanavade@ayanworks.com>
2024-03-08 21:36:12 +0530

    fix: env variable fixes
    Signed-off-by: pranalidhanavade <pranali.dhanavade@ayanworks.com>

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -8,33 +8,33 @@ const { PUBLIC_PLATFORM_NAME, PUBLIC_PLATFORM_LOGO, PUBLIC_POWERED_BY, PUBLIC_PL
 
 export const PLATFORM_DATA = {
 	name:
-		PUBLIC_PLATFORM_NAME || env.PUBLIC_PLATFORM_NAME,
+		PUBLIC_PLATFORM_NAME || env.PUBLIC_PLATFORM_NAME || "CREDEBL",
 	logo:
-		PUBLIC_PLATFORM_LOGO || env.PUBLIC_PLATFORM_LOGO,
+		PUBLIC_PLATFORM_LOGO || env.PUBLIC_PLATFORM_LOGO || "/images/CREDEBL_ICON.png",
 	poweredBy:
-		PUBLIC_POWERED_BY || env.PUBLIC_POWERED_BY,
+		PUBLIC_POWERED_BY || env.PUBLIC_POWERED_BY || "Blockster Labs Pvt. Ltd.",
 	webUrl:
 		PUBLIC_PLATFORM_WEB_URL ||
-		env.PUBLIC_PLATFORM_WEB_URL,
+		env.PUBLIC_PLATFORM_WEB_URL || "https://credebl.id/",
 	orgUrl:
 		PUBLIC_POWERED_BY_URL ||
-		env.PUBLIC_POWERED_BY_URL,
+		env.PUBLIC_POWERED_BY_URL || "https://blockster.global",
 	docs:
 		PUBLIC_PLATFORM_DOCS_URL ||
-		env.PUBLIC_PLATFORM_DOCS_URL,
+		env.PUBLIC_PLATFORM_DOCS_URL || "https://docs.credebl.id/en/intro/what-is-credebl/",
 	git:
 		PUBLIC_PLATFORM_GIT ||
-		env.PUBLIC_PLATFORM_GIT,
+		env.PUBLIC_PLATFORM_GIT || "https://github.com/credebl",
 	support:
     PUBLIC_PLATFORM_SUPPORT_EMAIL ||
-	env.PUBLIC_PLATFORM_SUPPORT_EMAIL,
+	env.PUBLIC_PLATFORM_SUPPORT_EMAIL || "support@blockster.global",
 
         twitter:
         PUBLIC_PLATFORM_TWITTER_URL ||
-		env.PUBLIC_PLATFORM_TWITTER_URL,
+		env.PUBLIC_PLATFORM_TWITTER_URL || "https://twitter.com/i/flow/login?redirect_after_login=%2Fcredebl",
         discord:
         PUBLIC_PLATFORM_DISCORD_URL ||
-		env.PUBLIC_PLATFORM_DISCORD_URL,
+		env.PUBLIC_PLATFORM_DISCORD_URL || "https://discord.gg/w4hnQT7NJG",
 	images: [
 		{
 			id: 'create_wallet',
