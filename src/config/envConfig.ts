ae0a500348dde968619389bfe41b4deec4585a40
sanjay.khatal <sanjay.khatal@ayanworks.com>
2024-03-09 15:14:42 +0530

    fix: access env
    Signed-off-by: sanjay.khatal <sanjay.khatal@ayanworks.com>

--- src/config/envConfig.ts
+++ src/config/envConfig.ts
@@ -4,7 +4,7 @@ console.log(34543543, globalThis);
 export const envConfig = {
 	PUBLIC_BASE_URL:
 		PUBLIC_BASE_URL || import.meta.env.PUBLIC_BASE_URL,
-	PUBLIC_CRYPTO_PRIVATE_KEY:
+			PUBLIC_CRYPTO_PRIVATE_KEY:
 		PUBLIC_CRYPTO_PRIVATE_KEY ||
 		import.meta.env.PUBLIC_CRYPTO_PRIVATE_KEY,
 	PUBLIC_PLATFORM_LOGO: logo || import.meta.env.PUBLIC_CRYPTO_PRIVATE_KEY
