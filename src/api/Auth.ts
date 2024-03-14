b73045d5e67312004198e485d883b92fe1204414
Nishad <nishad.shirsat@ayanworks.com>
2024-03-14 19:21:25 +0530

    removed unnecessary encryption while storing token in cookies
    Signed-off-by: Nishad <nishad.shirsat@ayanworks.com>

--- src/api/Auth.ts
+++ src/api/Auth.ts
@@ -273,7 +273,6 @@ export const setToCookies = (cookies: AstroCookies, key: string, value: any, opt
 		return;
 	}
     
-    const convertedValue = encryptData(value)
     // Set HttpOnly, Secure, and SameSite attributes in the options
     const updatedOption: { [key: string]: any }= {
         ...option,
@@ -281,15 +280,14 @@ export const setToCookies = (cookies: AstroCookies, key: string, value: any, opt
         secure: true, // Set to true if using HTTPS
         sameSite: 'Strict', 
       };
-    cookies.set(key, convertedValue as string, updatedOption)
+    cookies.set(key, value as string, updatedOption)
 
     return true
 }
 
 export const getFromCookies = (cookies: AstroCookies, key: string) =>{
     const value = cookies.get(key).value
-    const convertedValue = value ? decryptData(value) : ''
-    return convertedValue
+    return value
 }
 
 export const removeFromLocalStorage = async (key: string) => {
