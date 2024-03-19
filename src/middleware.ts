5e8cfcb15ec582676f2653e2919cfc3af0058302
sanjay.khatal <sanjay.khatal@ayanworks.com>
2024-03-19 17:33:54 +0530

    fix: middleware to add headers
    Signed-off-by: sanjay.khatal <sanjay.khatal@ayanworks.com>

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,58 +1,16 @@
-// export default async (request: Request, context: any) => {
-//     const response = await context.next();
-//     response.headers.set("X-Frame-Options", "SAMEORIGIN"); // Add your header
-//     response.headers.set('Content-Security-Policy',"script-src 'self'; frame-ancestors 'self'");
-//     response.headers.set("X-CREDEBL", "SSI")
-//     console.log("LOG FROM MIDDLEWARE::::::", response)
-//     return response;
-//   };
-
-// export function onRequest ({ locals, request }, next) {
-//   // intercept data from a request
-//   // optionally, modify the properties in `locals`
-//   locals.title = "New title";
-
-//   console.log(656565, locals, request)
-//   // return a Response or the result of calling `next()`
-//   return next();
-// };
-
-
 export const onRequest = async (context, next) => {
   const response = await next();
-  
   const html = await response.text();
-  // const redactedHtml = html.replaceAll("PRIVATE INFO", "REDACTED");
-  
-  console.log(656565, html, response.headers[Symbol.for('headers map')])
 
-const headers = response.headers
+  const allowedDomains = "https://fonts.googleapis.com/css2 https://www.ayanworks.com https://qaapi.credebl.id https://devapi.credebl.id https://fonts.googleapis.com https://avatars.githubusercontent.com https://fonts.gstatic.com/"
 
-if (!headers[Symbol.for('headers map')]) {
-  headers[Symbol.for('headers map')] = new Map();
-}
+  response.headers.set('Content-Security-Policy',`"default-src 'self'; script-src 'self' ${allowedDomains} theme global pkg; style-src 'unsafe-inline' ${allowedDomains}; font-src ${allowedDomains}; img-src 'self' ${allowedDomains}; frame-src 'self' ${allowedDomains}; object-src 'none'; media-src 'self'; connect-src 'self' ${allowedDomains};"`);
+  response.headers.set('X-Frame-Options', "DENY");
 
-// Set content-type property in the map
-headers[Symbol.for('headers map')].set('content-type', { name: 'content-type', value: 'text/html' });
-// // Set Access-Control-Allow-Origin property in the map
-// headers[Symbol.for('headers map')].set('Access-Control-Allow-Origin', { name: 'Access-Control-Allow-Origin', value: 'http://ayanworks.com' });
-// Set content-type property in the map
-headers[Symbol.for('headers map')].set('X-Frame-Options', { name: 'X-Frame-Options', value: 'DENY' });
-
-// Get content-type property value
-let contentTypeValue = null;
-if (headers[Symbol.for('headers map')]) {
-  contentTypeValue = headers[Symbol.for('headers map')].get('Access-Control-Allow-Origin')?.value;
-}
-
-// Logging the content-type property value
-console.log("Content-Type:", headers);
-
-console.log(6565652323, response.headers[Symbol.for('headers map')], headers[Symbol.for('headers map')].get('content-type')?.value)
-
+console.log("Headers:::", response.headers);
 
   return new Response(html, {
       status: 200,
-      headers: headers
+      headers: response.headers
   });
 };
