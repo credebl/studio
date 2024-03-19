46a435cb67721d2e8b34969f2d7169c86ec5b57c
Nishad <nishad.shirsat@ayanworks.com>
2024-03-19 18:01:06 +0530

    cosmetic changes for id
    Signed-off-by: Nishad <nishad.shirsat@ayanworks.com>

--- src/components/organization/SharedAgentForm.tsx
+++ src/components/organization/SharedAgentForm.tsx
@@ -465,7 +465,7 @@ const SharedAgentForm = ({
 										setSelectedDid('');
 										setGeneratedKeys(null);
 									}}
-									id="method"
+									id="did_method"
 									name="method"
 									className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 h-11"
 								>
@@ -564,7 +564,7 @@ const SharedAgentForm = ({
 												setSelectedDid('');
 											}}
 											value={selectedNetwork}
-											id="ledger"
+											id="did_ledger"
 											name="ledger"
 											className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 h-11"
 										>
