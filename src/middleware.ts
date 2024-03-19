e47d295389f0a65f31df1d2d08610a896f56cdcd
sanjay.khatal <sanjay.khatal@ayanworks.com>
2024-03-19 18:37:10 +0530

    fix: form-action header
    Signed-off-by: sanjay.khatal <sanjay.khatal@ayanworks.com>

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,8 +4,9 @@ export const onRequest = async (context, next) => {
 
   const allowedDomains = "https://fonts.googleapis.com/css2 https://www.ayanworks.com https://qaapi.credebl.id https://devapi.credebl.id https://fonts.googleapis.com https://avatars.githubusercontent.com https://fonts.gstatic.com/"
 
-  response.headers.set('Content-Security-Policy',`"default-src 'self'; script-src 'self' ${allowedDomains} theme global pkg; style-src 'unsafe-inline' ${allowedDomains}; font-src ${allowedDomains}; img-src 'self' ${allowedDomains}; frame-src 'self' ${allowedDomains}; object-src 'none'; media-src 'self'; connect-src 'self' ${allowedDomains};"`);
+  response.headers.set('Content-Security-Policy',`default-src 'self'; script-src 'self' ${allowedDomains}; style-src 'self' ${allowedDomains}; font-src ${allowedDomains}; img-src 'self' ${allowedDomains}; frame-src 'self' ${allowedDomains}; object-src 'none'; media-src 'self'; connect-src 'self' ${allowedDomains}; form-action 'self'; frame-ancestors 'self'; `);
   response.headers.set('X-Frame-Options', "DENY");
+  response.headers.set('X-Content-Type-Options', 'nosniff');
 
 console.log("Headers:::", response.headers);
 
