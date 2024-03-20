c44413b784c9693c474c3b5c5f9a43f5b2b69e91
Nishad <nishad.shirsat@ayanworks.com>
2024-03-20 14:51:57 +0530

    cosmetic changesin Field
    Signed-off-by: Nishad <nishad.shirsat@ayanworks.com>

--- src/components/organization/WalletSpinup.tsx
+++ src/components/organization/WalletSpinup.tsx
@@ -412,17 +412,10 @@ const SharedAgentForm = ({
 									<Label htmlFor="name" value="Wallet Label" />
 									<span className="text-red-500 text-xs">*</span>
 
-									<Field
-										id="label"
-										name="label"
-										className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-										type="text"
-									/>
-									{formikHandlers?.errors?.label &&
-										formikHandlers?.touched?.label && (
-											<span className="text-red-500 absolute text-xs">
-												{formikHandlers?.errors?.label}
-											</span>
+									<Field id="label" name="label" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+										type="text"/>
+									{formikHandlers?.errors?.label && formikHandlers?.touched?.label && (
+											<span className="text-red-500 absolute text-xs">{formikHandlers?.errors?.label}</span>
 										)}
 								</div>
 								<div className="w-full flex justify-end">
