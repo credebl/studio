162892879b58ffcfdffbf6a489a039b1ffcf880c
Nishad <nishad.shirsat@ayanworks.com>
2024-03-20 12:56:28 +0530

    cosmetic changes
    Signed-off-by: Nishad <nishad.shirsat@ayanworks.com>

--- src/components/organization/AlreadyDidSharedForm.tsx
+++ src/components/organization/AlreadyDidSharedForm.tsx
@@ -225,30 +225,20 @@ const AlreadyDidSharedForm: React.FC<DidSharedFormProps> = ({
 								)}
 
 							<div className="my-3 relative">
-								<label
-									htmlFor="DID Method" className="text-sm font-medium text-gray-900 dark:text-gray-300"
-								>
-									DID Method
-								</label>
+								<label htmlFor="DID Method" className="text-sm font-medium text-gray-900 dark:text-gray-300">DID Method</label>
 								<div className="bg-gray-100 font-semibold text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 h-11">
-									{showMethod(formikHandlers.values.method)}
-								</div>
+									{showMethod(formikHandlers.values.method)}</div>
 							</div>
 							<div className="mt-3 relative">
 								<Label htmlFor="name" value="Wallet Label" />
 								<span className="text-red-500 text-xs">*</span>
 
 								<Field
-									id="label"
-									name="label"
-									className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+									id="label" name="label"	className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
 									type="text"
 								/>
-								{formikHandlers?.errors?.label &&
-									formikHandlers?.touched?.label && (
-										<span className="text-red-500 absolute text-xs">
-											{formikHandlers?.errors?.label}
-										</span>
+								{formikHandlers?.errors?.label && formikHandlers?.touched?.label && (
+										<span className="text-red-500 absolute text-xs"> {formikHandlers?.errors?.label}</span>
 									)}
 							</div>
 							<div className="w-full flex justify-end">
