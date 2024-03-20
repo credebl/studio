d856bc0f3f69f3d821927f3093f4c6c5d970eb36
sanjay.khatal <sanjay.khatal@ayanworks.com>
2024-03-20 13:27:31 +0530

    refactor: CSP
    Signed-off-by: sanjay.khatal <sanjay.khatal@ayanworks.com>

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,4 @@
-export const onRequest = async (context, next) => {
+export const onRequest = async (context: any, next: any) => {
   const response = await next();
   const html = await response.text();
  
@@ -9,12 +9,13 @@ export const onRequest = async (context, next) => {
 
   // response.headers.set('Content-Security-Policy',`default-src 'self'; script-src 'self' ${allowedDomains} 'nonce-${nonce}_scripts'; style-src 'self' ${allowedDomains} 'nonce-${nonce}_styles'; font-src ${allowedDomains}; img-src 'self' ${allowedDomains}; frame-src 'self' ${allowedDomains}; object-src 'none'; media-src 'self'; connect-src 'self' ${allowedDomains}; form-action 'self'; frame-ancestors 'self'; `);
   
-  response.headers.set('Content-Security-Policy',`default-src 'self'; font-src ${allowedDomains}; img-src 'self' ${allowedDomains}; frame-src 'self' ${allowedDomains}; object-src 'none'; media-src 'self'; connect-src 'self' ${allowedDomains}; form-action 'self'; frame-ancestors 'self'; `);
+  // response.headers.set('Content-Security-Policy',`default-src 'self'; font-src ${allowedDomains}; img-src 'self' ${allowedDomains}; frame-src 'self' ${allowedDomains}; object-src 'none'; media-src 'self'; connect-src 'self' ${allowedDomains}; form-action 'self'; frame-ancestors 'self'; `);
   response.headers.set('X-Frame-Options', "DENY");
   response.headers.set('X-Content-Type-Options', 'nosniff');
   response.headers.set('Access-Control-Allow-Origin', allowedDomains)
   response.headers.set('ServerTokens', 'Prod')
   response.headers.set('server_tokens', 'off')
+  response.headers.set('server', 'CustomServer')
   response.headers.set('Server', 'CustomServer')
 
   // let updatedHtml = await html.split("<script").join(`<script nonce="${nonce}_scripts"`).split("<style").join(`<style nonce="${nonce}_styles" `).split("style=\"").join(`nonce="${nonce}_styles" style="`);
