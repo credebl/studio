feeb1918215ac3dc28ea38cbdc0e5a72fe04f55b
sanjay.khatal <sanjay.khatal@ayanworks.com>
2024-03-27 12:23:27 +0530

    refactor: logs, csp directives, axios refresh token
    Signed-off-by: sanjay.khatal <sanjay.khatal@ayanworks.com>

--- src/commonComponents/EcosystemProfileCard.tsx
+++ src/commonComponents/EcosystemProfileCard.tsx
@@ -107,8 +107,9 @@ const EcosystemProfileCard = ({getEndorsementListData}:IEndorsement) => {
                             <select
                                 className="mb-4 bg-gray-50 sm:min-w-[244px] border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                 onChange={handleSelectEcosystem}
+                                defaultValue={""}
                             >
-                                <option selected>Select Ecosystem</option>
+                                <option value="">Select Ecosystem</option>
                                 {
                                     ecosystemOptions && ecosystemOptions?.map(item => (
                                         <option key={item?.id} value={item?.id}>{item?.name}</option>
