49a3da5b772bad5650ca45361de7eacca5c1df95
pranalidhanavade <pranali.dhanavade@ayanworks.com>
2024-03-27 12:49:41 +0530

    fix: unexpected authorization error
    Signed-off-by: pranalidhanavade <pranali.dhanavade@ayanworks.com>

--- src/api/Auth.ts
+++ src/api/Auth.ts
@@ -229,7 +229,10 @@ export const encryptData = (value: any): string => {
     const CRYPTO_PRIVATE_KEY: string = `${envConfig.PUBLIC_CRYPTO_PRIVATE_KEY}`
 
     try {
-        const encJson: string = CryptoJS.AES.encrypt(JSON.stringify(value), CRYPTO_PRIVATE_KEY).toString();
+        if(typeof(value) !== 'string'){
+            value = JSON.stringify(value)
+        }
+        const encJson: string = CryptoJS.AES.encrypt(value, CRYPTO_PRIVATE_KEY).toString();
         let encData = CryptoJS.enc.Base64.stringify(CryptoJS.enc.Utf8.parse(encJson))
 
         return encData;
@@ -247,11 +250,17 @@ export const decryptData = (value: any): string => {
 
         let decData = CryptoJS.enc.Base64.parse(value).toString(CryptoJS.enc.Utf8)
         let bytes = CryptoJS.AES.decrypt(decData, CRYPTO_PRIVATE_KEY).toString(CryptoJS.enc.Utf8)
-        const parsedData = JSON.parse(bytes);
-        if (typeof parsedData !== 'string') {
-			return JSON.stringify(parsedData);
+        
+        try {
+            bytes = JSON.parse(bytes);
+        } catch (error) {
+            
+        }
+
+        if (typeof bytes !== 'string') {
+			return JSON.stringify(bytes);
 		}
-        return parsedData;
+        return bytes;
     } catch (error) {
         // Handle decryption error or invalid input
         console.error('Decryption error:', error);
