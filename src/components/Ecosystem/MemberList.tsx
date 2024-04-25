ce7fd1ed9034be1eff2230bb36359813d1bf2752
bhavanakarwade <bhavana.karwade@ayanworks.com>
2024-04-25 12:13:25 +0530

    fix: role wise list access
    Signed-off-by: bhavanakarwade <bhavana.karwade@ayanworks.com>

--- src/components/Ecosystem/MemberList.tsx
+++ src/components/Ecosystem/MemberList.tsx
@@ -21,6 +21,7 @@ const initialPageState = {
 
 interface IMemberList {
 	orgId: string;
+	ecosystemId: string;
 	ecosystem: { createDateTime: string };
 	ecosystemRole: { name: string };
 	organisation: {
@@ -90,7 +91,10 @@ const MemberList = () => {
 	};
 
 	const getEcosystemMembers = async (apiParameter: IMemberListAPIParameter) => {
-		const userOrgId = await getFromLocalStorage(storageKeys.ORG_ID);
+		const userProfile = await getFromLocalStorage(storageKeys.USER_PROFILE)
+		const parsedUserProfileData = JSON.parse(userProfile);
+		const userId = parsedUserProfileData.id;
+		
 		setLoading(true);
 		const response = await getEcosystemMemberList(apiParameter);
 		const { data } = response as AxiosResponse;
@@ -106,7 +110,7 @@ const MemberList = () => {
 			});
 			const memberList =
 				data?.data?.data?.length > 0 &&
-				data?.data?.data?.map((member: IMemberList) => {
+				data?.data?.data?.map((member) => {
 					let orgDid = 'Not available';
 					if (member.organisation.org_agents.length > 0) {
 						const orgAgent = member.organisation.org_agents[0];
@@ -151,7 +155,8 @@ const MemberList = () => {
 										>
 											{member?.ecosystemRole?.name}
 										</span>
-										{member?.orgId === userOrgId ? '(You)' : ''}
+										{member.organisation.userOrgRoles.some((item) => item.userId === userId) ? '(You)' : ''}
+
 									</span>
 								) : (
 									'Not available'
