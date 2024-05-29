5485693ed92f2b2705fc5d4424d0cfbaecbf36cf
pranalidhanavade <pranali.dhanavade@ayanworks.com>
2024-05-29 17:59:47 +0530

    fix: type errors
    Signed-off-by: pranalidhanavade <pranali.dhanavade@ayanworks.com>

--- src/commonComponents/EcosystemProfileCard.tsx
+++ src/commonComponents/EcosystemProfileCard.tsx
@@ -86,9 +86,9 @@ const EcosystemProfileCard = ({getEndorsementListData}:IEndorsement) => {
                 >
                     <div className="mr-4">
                         {ecosystemDetails?.logoUrl ? (
-                            <CustomAvatar size="80px" src={ecosystemDetails?.logoUrl} />
+                            <CustomAvatar size="80px" round src={ecosystemDetails?.logoUrl} />
                         ) : (
-                            <CustomAvatar size="80px" name={ecosystemDetails?.name} />
+                            <CustomAvatar size="80px" round name={ecosystemDetails?.name} />
                         )}
                     </div>
 
