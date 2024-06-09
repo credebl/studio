bfb09e32b969e043ba7c34849e1effbd5f252972
pranalidhanavade <pranali.dhanavade@ayanworks.com>
2024-06-09 16:32:20 +0530

    fix:sonarcloud issues
    Signed-off-by: pranalidhanavade <pranali.dhanavade@ayanworks.com>

--- src/components/Authentication/ResetPassword.tsx
+++ src/components/Authentication/ResetPassword.tsx
@@ -26,6 +26,37 @@ interface passwordValues {
 	confirmPassword: string;
 }
 
+
+const EyeIconVisible = () => (
+	<svg
+		className="h-6 w-6 text-black"
+		viewBox="0 0 24 24"
+		fill="none"
+		stroke="currentColor"
+		strokeWidth="2"
+		strokeLinecap="round"
+		strokeLinejoin="round"
+	>
+		<path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z" />
+		<circle cx="12" cy="12" r="3" />
+	</svg>
+);
+
+const EyeIconInvisible = () => (
+	<svg
+		className="h-6 w-6 text-black"
+		viewBox="0 0 24 24"
+		fill="none"
+		stroke="currentColor"
+		strokeWidth="2"
+		strokeLinecap="round"
+		strokeLinejoin="round"
+	>
+		<path d="M17.94 17.94A10.07 10.07 0 0 1 12 20c-7 0-11-8-11-8a18.45 18.45 0 0 1 5.06-5.94M9.9 4.24A9.12 9.12 0 0 1 12 4c7 0 11 8 11 8a18.5 18.5 0 0 1-2.16 3.19m-6.72-1.07a3 3 0 1 1-4.24-4.24" />
+		<line x1="1" y1="1" x2="23" y2="23" />
+	</svg>
+);
+
 const ResetPassword = () => {
 	const [loading, setLoading] = useState<boolean>(false);
 	const [erroMsg, setErrMsg] = useState<string | null>(null);
@@ -189,31 +220,10 @@ const ResetPassword = () => {
 														className="bg-transparent absolute right-2 top-1/2 transform -translate-y-1/2 text-gray-500 dark:text-white hover:text-gray-800 dark:hover:text-white"
 													>
 														{passwordVisible ? (
-															<svg
-															className="h-6 w-6 text-black"
-															viewBox="0 0 24 24"
-															fill="none"
-															stroke="currentColor"
-															stroke-width="2"
-															stroke-linecap="round"
-															stroke-linejoin="round"
-														>
-															<path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z" />
-															<circle cx="12" cy="12" r="3" />
-														</svg>
+															<EyeIconVisible />
 														) : (
-															<svg
-																className="h-6 w-6 text-black"
-																viewBox="0 0 24 24"
-																fill="none"
-																stroke="currentColor"
-																stroke-width="2"
-																stroke-linecap="round"
-																stroke-linejoin="round"
-															>
-																<path d="M17.94 17.94A10.07 10.07 0 0 1 12 20c-7 0-11-8-11-8a18.45 18.45 0 0 1 5.06-5.94M9.9 4.24A9.12 9.12 0 0 1 12 4c7 0 11 8 11 8a18.5 18.5 0 0 1-2.16 3.19m-6.72-1.07a3 3 0 1 1-4.24-4.24" />
-																<line x1="1" y1="1" x2="23" y2="23" />
-															</svg>
+															<EyeIconInvisible />
+
 														)}
 													</button>
 												</div>
