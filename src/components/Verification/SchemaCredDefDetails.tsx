84528f34b3ee048c98f047e7e850cea3592f6bf2
pranalidhanavade <pranali.dhanavade@ayanworks.com>
2024-08-13 10:12:41 +0530

    fix: sonarlint issues
    Signed-off-by: pranalidhanavade <pranali.dhanavade@ayanworks.com>

--- src/components/Verification/SchemaCredDefDetails.tsx
+++ src/components/Verification/SchemaCredDefDetails.tsx
@@ -1,3 +1,95 @@
+// import CopyDid from "../../commonComponents/CopyDid"
+
+// interface SchemaCredDefProps {
+//     schemaCredDefList: {
+//         name: string;
+//         credDefId?: string; 
+//         schemaId: string; 
+//     }[];
+// }
+
+
+// const SchemaCredDefDetails = ({ schemaCredDefList }: SchemaCredDefProps) => {
+
+//     return (
+//         <>
+//         {schemaCredDefList[0].schemaId && !schemaCredDefList[0].credDefId && schemaCredDefList.map((item) => (
+//                 <div key={Object.values(item)[2]} className="flex justify-start ml-2 w-full mt-6">
+//                     <div className="w-full">
+//                         {Object.values(item)[1] ? (
+//                             <div className="flex flex-start mb-2 w-full ">
+//                                 <div className="w-3/12 font-semibold text-primary-700 dark:bg-gray-800 m-1 p-1 flex justify-start items-center text-start">
+//                                     {Object.values(item)[1] ? 'Schema Id' : ''}
+//                                 </div>{' '}
+//                                 <div className="flex items-center p-1 m-1">
+//                                     {' '}
+//                                     :
+//                                 </div>{' '}
+//                                 <div className="w-9/12 m-1 text-start text-gray-600 dark:text-white items-center cursor-pointer overflow-auto">
+//                                     <div className="flex w-full">
+//                                         <CopyDid value={Object.values(item)[1] || ''} className='truncate font-courier mt-2' />
+//                                     </div>
+//                                 </div>
+//                             </div>
+//                         ) : (
+//                             ''
+//                         )}
+//                     </div>
+//                 </div>
+//             ))}
+
+//             {schemaCredDefList[0].schemaId && schemaCredDefList[0].credDefId && schemaCredDefList.map((item) => (
+//                 <div key={Object.values(item)[2]} className="flex justify-start ml-2 w-full mt-6">
+//                     <div className="w-full">
+//                         {Object.values(item)[2] ? (
+//                             <div className="flex flex-start mb-2 w-full ">
+//                                 <div className="w-3/12 font-semibold text-primary-700 dark:bg-gray-800 m-1 p-1 text-start">
+//                                     {Object.values(item)[2] ? 'Schema Id' : ''}
+//                                 </div>{' '}
+//                                 <div className=" p-1 m-1">
+//                                     {' '}
+//                                     :
+//                                 </div>{' '}
+//                                 <div className="w-9/12 m-1 text-start text-gray-600 dark:text-white items-center cursor-pointer overflow-auto">
+//                                     <div className="flex w-full">
+//                                         <CopyDid value={Object.values(item)[2] || ''} className='truncate font-courier mt-2' />
+//                                     </div>
+//                                 </div>
+//                             </div>
+//                         ) : (
+//                             ''
+//                         )}
+
+//                         {Object.values(item)[1] ? (
+//                             <div className="flex flex-start mb-2 w-full ">
+//                                 <div className="w-3/12 font-semibold text-primary-700 dark:bg-gray-800 m-1 p-1 flex justify-start items-center text-start">
+//                                     {Object.values(item)[1] ? 'CredDef Id' : ''}
+//                                 </div>{' '}
+//                                 <div className="flex items-center p-1 m-1">
+//                                     {' '}
+//                                     :
+//                                 </div>{' '}
+//                                 <div className="w-9/12 m-1 text-start text-gray-600 dark:text-white items-center cursor-pointer overflow-auto">
+//                                     <div className="flex w-full">
+//                                         <CopyDid value={Object.values(item)[1] || ''} className='truncate font-courier mt-2' />
+//                                     </div>
+//                                 </div>
+//                             </div>
+//                         ) : (
+//                             ''
+//                         )}
+//                     </div>
+//                 </div>
+//             ))}
+            
+            
+//         </>
+//     )
+// }
+
+// export default SchemaCredDefDetails
+
+
 import CopyDid from "../../commonComponents/CopyDid"
 
 interface SchemaCredDefProps {
@@ -8,83 +100,42 @@ interface SchemaCredDefProps {
     }[];
 }
 
+// Component to display a label and its associated value
+const IdDisplay = ({ label, value }: { label: string; value: string }) => (
+    <div className="flex flex-start mb-2 w-full">
+        <div className="w-3/12 font-semibold text-primary-700 dark:bg-gray-800 m-1 p-1 flex justify-start items-center text-start">
+            {label}
+        </div>
+        <div className="flex items-center p-1 m-1">:</div>
+        <div className="w-9/12 m-1 text-start text-gray-600 dark:text-white items-center cursor-pointer overflow-auto">
+            <div className="flex w-full">
+                <CopyDid value={value} className='truncate font-courier mt-2' />
+            </div>
+        </div>
+    </div>
+);
 
 const SchemaCredDefDetails = ({ schemaCredDefList }: SchemaCredDefProps) => {
-
     return (
         <>
-        {schemaCredDefList[0].schemaId && !schemaCredDefList[0].credDefId && schemaCredDefList.map((item) => (
-                <div key={Object.values(item)[2]} className="flex justify-start ml-2 w-full mt-6">
+            {schemaCredDefList.map((item, index) => (
+                <div key={item.schemaId} className="flex justify-start ml-2 w-full mt-6">
                     <div className="w-full">
-                        {Object.values(item)[1] ? (
-                            <div className="flex flex-start mb-2 w-full ">
-                                <div className="w-3/12 font-semibold text-primary-700 dark:bg-gray-800 m-1 p-1 flex justify-start items-center text-start">
-                                    {Object.values(item)[1] ? 'Schema Id' : ''}
-                                </div>{' '}
-                                <div className="flex items-center p-1 m-1">
-                                    {' '}
-                                    :
-                                </div>{' '}
-                                <div className="w-9/12 m-1 text-start text-gray-600 dark:text-white items-center cursor-pointer overflow-auto">
-                                    <div className="flex w-full">
-                                        <CopyDid value={Object.values(item)[1] || ''} className='truncate font-courier mt-2' />
-                                    </div>
-                                </div>
-                            </div>
-                        ) : (
-                            ''
+                        {item.schemaId && !item.credDefId && (
+                            <IdDisplay label="Schema Id" value={item.schemaId} />
+                        )}
+                        {item.schemaId && item.credDefId && (
+                            <>
+                                <IdDisplay label="Schema Id" value={item.schemaId} />
+                                <IdDisplay label="CredDef Id" value={item.credDefId} />
+                            </>
                         )}
                     </div>
                 </div>
             ))}
-            
-            {schemaCredDefList[0].schemaId && schemaCredDefList[0].credDefId && schemaCredDefList.map((item) => (
-                <div key={Object.values(item)[2]} className="flex justify-start ml-2 w-full mt-6">
-                    <div className="w-full">
-                        {Object.values(item)[2] ? (
-                            <div className="flex flex-start mb-2 w-full ">
-                                <div className="w-3/12 font-semibold text-primary-700 dark:bg-gray-800 m-1 p-1 text-start">
-                                    {Object.values(item)[2] ? 'Schema Id' : ''}
-                                </div>{' '}
-                                <div className=" p-1 m-1">
-                                    {' '}
-                                    :
-                                </div>{' '}
-                                <div className="w-9/12 m-1 text-start text-gray-600 dark:text-white items-center cursor-pointer overflow-auto">
-                                    <div className="flex w-full">
-                                        <CopyDid value={Object.values(item)[2] || ''} className='truncate font-courier mt-2' />
-                                    </div>
-                                </div>
-                            </div>
-                        ) : (
-                            ''
-                        )}
-
-                        {Object.values(item)[1] ? (
-                            <div className="flex flex-start mb-2 w-full ">
-                                <div className="w-3/12 font-semibold text-primary-700 dark:bg-gray-800 m-1 p-1 flex justify-start items-center text-start">
-                                    {Object.values(item)[1] ? 'CredDef Id' : ''}
-                                </div>{' '}
-                                <div className="flex items-center p-1 m-1">
-                                    {' '}
-                                    :
-                                </div>{' '}
-                                <div className="w-9/12 m-1 text-start text-gray-600 dark:text-white items-center cursor-pointer overflow-auto">
-                                    <div className="flex w-full">
-                                        <CopyDid value={Object.values(item)[1] || ''} className='truncate font-courier mt-2' />
-                                    </div>
-                                </div>
-                            </div>
-                        ) : (
-                            ''
-                        )}
-                    </div>
-                </div>
-            ))}
-            
-            
         </>
-    )
+    );
 }
 
-export default SchemaCredDefDetails
+export default SchemaCredDefDetails;
+
