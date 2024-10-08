73207332552066a2e03be28c79a57c82dd6ef899
pranalidhanavade <pranali.dhanavade@ayanworks.com>
2024-10-08 21:31:11 +0530

    refactor: alertcomponent
    Signed-off-by: pranalidhanavade <pranali.dhanavade@ayanworks.com>

--- src/components/AlertComponent/index.tsx
+++ src/components/AlertComponent/index.tsx
@@ -25,58 +25,57 @@ export const AlertComponent = ({
 	return (
 		message !== null ?
 		 (
-		 <>
-		</>
-		)
-		:
-		 (
-			<div className="w-full">
-				<div
-					className={`flex flex-col gap-2 p-4 text-sm rounded-lg mb-4 ${() => getAlertClass(type)}`}
-					role="alert"
-				>
+			(
+				<div className="w-full">
 					<div
-						className="flex items-center w-full"
-						data-testid="flowbite-alert-wrapper"
+						className={`flex flex-col gap-2 p-4 text-sm rounded-lg mb-4 ${() => getAlertClass(type)}`}
+						role="alert"
 					>
-						<div className="w-full">
-							<div className="flex flex-wrap justify-between items-center w-full">
-								<div>{message}</div>
-								{viewButton && (
-									<div className="md:w-32 lg:w-48 text-base text-primary-700 text-right flex justify-end mr-2">
-										<a href={path}>View more... </a>
-									</div>
-								)}
-							</div>
-						</div>
-						<button
-							aria-label="Dismiss"
-							className={`-mx-1.5 -my-1.5 ml-auto inline-flex h-8 w-8 rounded-lg p-1.5 ${() => getAlertClass(type)}`}
-							type="button"
-							onClick={onAlertClose}
+						<div
+							className="flex items-center w-full"
+							data-testid="flowbite-alert-wrapper"
 						>
-							<svg
-								stroke="currentColor"
-								fill="currentColor"
-								strokeWidth="0"
-								viewBox="0 0 20 20"
-								aria-hidden="true"
-								className={`w-5 h-5 ${() => getAlertClass(type)}`}
-								height="1em"
-								width="1em"
-								xmlns="http://www.w3.org/2000/svg"
+							<div className="w-full">
+								<div className="flex flex-wrap justify-between items-center w-full">
+									<div>{message}</div>
+									{viewButton && (
+										<div className="md:w-32 lg:w-48 text-base text-primary-700 text-right flex justify-end mr-2">
+											<a href={path}>View more... </a>
+										</div>
+									)}
+								</div>
+							</div>
+							<button
+								aria-label="Dismiss"
+								className={`-mx-1.5 -my-1.5 ml-auto inline-flex h-8 w-8 rounded-lg p-1.5 ${() => getAlertClass(type)}`}
+								type="button"
+								onClick={onAlertClose}
 							>
-								<path
-									fillRule="evenodd"
-									d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-									clipRule="evenodd"
-								></path>
-							</svg>
-						</button>
+								<svg
+									stroke="currentColor"
+									fill="currentColor"
+									strokeWidth="0"
+									viewBox="0 0 20 20"
+									aria-hidden="true"
+									className={`w-5 h-5 ${() => getAlertClass(type)}`}
+									height="1em"
+									width="1em"
+									xmlns="http://www.w3.org/2000/svg"
+								>
+									<path
+										fillRule="evenodd"
+										d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+										clipRule="evenodd"
+									></path>
+								</svg>
+							</button>
+						</div>
 					</div>
 				</div>
-			</div>
-			
+				
+			)
 		)
+		:
+		 (<></>)
 	);
 };
