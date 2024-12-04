5e252e0c8e9350b3bc32a3ae54ab4df1261808d4
bhavanakarwade <bhavana.karwade@ayanworks.com>
2024-12-04 18:36:08 +0530

    fix: user profile values
    Signed-off-by: bhavanakarwade <bhavana.karwade@ayanworks.com>

--- src/components/Profile/DisplayUser.tsx
+++ src/components/Profile/DisplayUser.tsx
@@ -9,33 +9,16 @@ const DisplayUser = () => {
     const [userObj, setUserObj] = useState(null)
 		
     let timer:any= null
-    // const getUserDetails = async () => {
-    //     const userProfile = await getFromLocalStorage(storageKeys.USER_PROFILE)
-    //     const orgRoles = await getFromLocalStorage(storageKeys.ORG_ROLES)
-    //     const parsedUser = userProfile ? JSON.parse(userProfile) : null
-
-    //      if (parsedUser) {
-    //         parsedUser.roles = orgRoles;
-    //         setUserObj(parsedUser);
-    //     }
-    // }
-
     const getUserDetails = async () => {
-        try {
-            const userProfile = await getFromLocalStorage(storageKeys.USER_PROFILE);
-            const orgRoles = await getFromLocalStorage(storageKeys.ORG_ROLES);
-            const parsedUser = userProfile ? JSON.parse(userProfile) : null;
+        const userProfile = await getFromLocalStorage(storageKeys.USER_PROFILE)
+        const orgRoles = await getFromLocalStorage(storageKeys.ORG_ROLES)
+        const parsedUser = userProfile ? JSON.parse(userProfile) : null
 
-            if (parsedUser) {
-                parsedUser.roles = orgRoles || []; // Default to an empty array if orgRoles is null
-                setUserObj(parsedUser);
-            } else {
-                console.warn("User profile is not available in local storage.");
-            }
-        } catch (error) {
-            console.error("Error fetching user details:", error);
+         if (parsedUser) {
+            parsedUser.roles = orgRoles;
+            setUserObj(parsedUser);
         }
-    };
+    }
     
     useEffect(() => {
 			const fetchData = async () => {
