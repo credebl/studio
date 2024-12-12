12ee909250550d872e6974c85392f88db6a6fc28
bhavanakarwade <bhavana.karwade@ayanworks.com>
2024-12-12 13:59:30 +0530

    added import meta env file

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,10 +1,10 @@
-/// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
 
 // https://docs.astro.build/en/guides/environment-variables/#intellisense-for-typescript
 interface ImportMetaEnv {
 	readonly SITE: string;
 	readonly PUBLIC_BASE_URL:string;
+	readonly PUBLIC_CRYPTO_PRIVATE_KEY:string;
 }
 
 interface ImportMeta {
