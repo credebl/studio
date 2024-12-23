4e2372ed92e477d2d8ece19290054c91b5d91594
bhavanakarwade <bhavana.karwade@ayanworks.com>
2024-12-23 14:09:18 +0530

    refactor: improve variables initialization logic
    Signed-off-by: bhavanakarwade <bhavana.karwade@ayanworks.com>

--- src/services/axiosIntercepter.ts
+++ src/services/axiosIntercepter.ts
@@ -6,7 +6,7 @@ import { getFromLocalStorage, setToLocalStorage } from '../api/Auth';
 import { apiStatusCodes, storageKeys } from '../config/CommonConstant';
 
 const instance = axios.create({
-	baseURL: envConfig.PUBLIC_BASE_URL,
+	baseURL: import.meta.env.PUBLIC_BASE_URL,
 });
 
 const EcosystemInstance = axios.create({
@@ -16,7 +16,7 @@ const EcosystemInstance = axios.create({
 const checkAuthentication = async (sessionCookie: string, request: AxiosRequestConfig) => {
 	const isAuthPage = window.location.href.includes('/authentication/sign-in') || window.location.href.includes('/authentication/sign-up')
 	try {
-		const baseURL = envConfig.PUBLIC_BASE_URL || process.env.PUBLIC_BASE_URL;
+		const baseURL = import.meta.env.PUBLIC_BASE_URL || process.env.PUBLIC_BASE_URL;
 		const config = {
 			headers: {
 				'Content-Type': 'application/json',
