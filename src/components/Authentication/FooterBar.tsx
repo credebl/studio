d770ffc7b1c1f7a3e6633a37b704dc476695d2e4
Ankita Patidar <ankita.patidar@ayanworks.com>
2025-01-27 13:34:49 +0530

    docs: update case of CREDEBL
    Signed-off-by: Ankita Patidar <ankita.patidar@ayanworks.com>

--- src/components/Authentication/FooterBar.tsx
+++ src/components/Authentication/FooterBar.tsx
@@ -5,8 +5,8 @@ const FooterBar = () => {
         <footer className="bg-white dark:bg-gray-800">
             <div className="md:flex md:items-center md:justify-between p-3">
                 <p className="text-sm text-center text-gray-500">
-                    &copy; Credebl a Series of LF Projects, LLC | For web site terms of use, trademark policy and other project policies please see
-                    <a href="https://lfprojects.org/" className="hover:underline" target="_blank"
+                    &copy; CREDEBL a Series of LF Projects, LLC | For web site terms of use, trademark policy and other project policies please see
+                    <a href="https://lfprojects.org/" className="underline" target="_blank"
                     > https://lfprojects.org
                     </a>
                 </p>
