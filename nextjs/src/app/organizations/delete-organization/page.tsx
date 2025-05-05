f5e1c585c92035542f2209179d62f5aaa5cefc20
pranalidhanavade <pranali.dhanavade@ayanworks.com>
2025-05-05 17:55:19 +0530

    refactor: create, edit and delete organization components
    Signed-off-by: pranalidhanavade <pranali.dhanavade@ayanworks.com>

---
+++ nextjs/src/app/organizations/delete-organization/page.tsx
@@ -0,0 +1,13 @@
+import DeleteOrganizationPage from '@/features/organization/components/DeleteOrganization';
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+
+      <DeleteOrganizationPage />
+    </div>
+  )
+}
+
+export default page;
