5df66cec1a82f4ca18a529fde8b86ff7ebb06b63
Krishna Waske <krishna.waske@ayanworks.com>
2025-05-08 19:06:13 +0530

    fix: minor changes
    Signed-off-by: Krishna Waske <krishna.waske@ayanworks.com>

--- nextjs/src/components/ui/generic-table-component/data-table-toolbar.tsx
+++ nextjs/src/components/ui/generic-table-component/data-table-toolbar.tsx
@@ -47,7 +47,7 @@ export function DataTableToolbar<TData>({
     <div className='flex items-center justify-between'>
       <div className='flex flex-1 flex-col-reverse items-start gap-y-2 sm:flex-row sm:items-center sm:space-x-2'>
         <Input
-          placeholder='Filter tasks...'
+          placeholder='Filter data...'
           value={localValue}
           onChange={(e) => {
 						setLocalValue(e.target.value)}}
