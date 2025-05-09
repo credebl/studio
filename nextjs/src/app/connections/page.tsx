c8f1af9071d1f0757ab559b6ea5acd69541af8c5
Krishna Waske <krishna.waske@ayanworks.com>
2025-05-09 11:04:45 +0530

    fix: sonarcloud and other formating issues
    Signed-off-by: Krishna Waske <krishna.waske@ayanworks.com>

--- nextjs/src/app/connections/page.tsx
+++ nextjs/src/app/connections/page.tsx
@@ -1,12 +1,9 @@
-import PageContainer from '@/components/layout/page-container';
 import Connections from '@/features/connections';
 import React from 'react';
 
 const page = () => {
 	return (
-    <PageContainer>
       <Connections />
-    </PageContainer>
   );
 };
 
