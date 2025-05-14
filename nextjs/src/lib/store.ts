d68d009ca5db38cab82cca4118068db38050dbb6
Sujit <sujit.sutar@ayanworks.com>
2025-05-14 16:15:33 +0530

    fix: latest changes
    Signed-off-by: Sujit <sujit.sutar@ayanworks.com>

--- nextjs/src/lib/store.ts
+++ nextjs/src/lib/store.ts
@@ -38,4 +38,4 @@ export const persistor = persistStore(store)
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
-export type AppStore = typeof store
+export type AppStore = typeof store 
