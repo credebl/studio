3abbc19f5c7279f7d16ccfc5bc634211ad801d62
Sujit <sujit.sutar@ayanworks.com>
2025-05-19 13:25:51 +0530

    fix: minor fix to pages removed unecessary div's
    Signed-off-by: Sujit <sujit.sutar@ayanworks.com>

--- nextjs/src/app/organizations/credentials/connections/page.tsx
+++ nextjs/src/app/organizations/credentials/connections/page.tsx
@@ -1,10 +1,6 @@
 import Connections from '@/features/organization/connectionIssuance/components/Connections'
 import React from 'react'
 
-const page = (): React.JSX.Element => (
-  <div>
-    <Connections />
-  </div>
-)
+const page = (): React.JSX.Element => <Connections />
 
 export default page
--- nextjs/src/app/organizations/credentials/page.tsx
+++ nextjs/src/app/organizations/credentials/page.tsx
@@ -2,10 +2,6 @@ import React, { JSX } from 'react'
 
 import Credentials from '@/features/organization/connectionIssuance/components/Credentials'
 
-const page = (): JSX.Element => (
-  <div>
-    <Credentials />
-  </div>
-)
+const page = (): JSX.Element => <Credentials />
 
 export default page
