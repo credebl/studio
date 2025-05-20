46bbb7e6a47558ba1cc1b6db14f9639bafce6d15
pranalidhanavade <pranali.dhanavade@ayanworks.com>
2025-05-20 15:04:10 +0530

    fix: UI issue
    Signed-off-by: pranalidhanavade <pranali.dhanavade@ayanworks.com>

---
+++ nextjs/src/lib/store.ts
@@ -0,0 +1,48 @@
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import { persistReducer, persistStore } from 'redux-persist'
+
+import authSlice from './authSlice'
+import orgSlice from './orgSlice'
+import profileSlice from './profileSlice'
+import storage from 'redux-persist/lib/storage'
+import storageReducer from './storageKeys'
+import userSlice from './userSlice'
+import walletSpinupSlice from './walletSpinupSlice'
+
+const rootReducer = combineReducers({
+  auth: authSlice,
+  profile: profileSlice,
+  organization: orgSlice,
+  storageKeys: storageReducer,
+  user: userSlice,
+  wallet: walletSpinupSlice,
+})
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: [
+    'auth',
+    'profile',
+    'organization',
+    'storageKeys',
+    'user',
+    'wallet',
+  ],
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+})
+
+export const persistor = persistStore(store)
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
+export type AppStore = typeof store
