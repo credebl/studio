57542dd5698ba8210c997bd410387d416cb7e7cb
Sujit <sujit.sutar@ayanworks.com>
2025-05-31 21:18:31 +0530

    fix/ ellipse comments
    Signed-off-by: Sujit <sujit.sutar@ayanworks.com>

--- nextjs/src/features/schemas/components/ConfirmationModal.tsx
+++ nextjs/src/features/schemas/components/ConfirmationModal.tsx
@@ -81,7 +81,7 @@ const ConfirmationModal = ({
         </div>
       )}
 
-      <DialogFooter className="mt-6 flex flex-col justify-center! gap-4 sm:flex-row">
+      <DialogFooter className="m-auto mt-6 flex flex-col gap-4 sm:flex-row">
         <Button
           variant="outline"
           onClick={() => closeModal(false)}
