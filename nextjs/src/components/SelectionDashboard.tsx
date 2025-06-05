6824da4fba3ed271a325fe87cdcb29a5ba8b8cad
sanjaykhondal-aw <sanjay.khondal@ayanworks.com>
2025-06-05 14:35:36 +0530

    Removed inline css
    Signed-off-by: sanjaykhondal-aw <sanjay.khondal@ayanworks.com>

--- nextjs/src/components/SelectionDashboard.tsx
+++ nextjs/src/components/SelectionDashboard.tsx
@@ -30,7 +30,7 @@ const SelectionDashboard = ({ title, options }: IDashboard): JSX.Element => {
         </h1>
         <BackButton />
       </div>
-      <div className="rounded-lg border bg-white px-6 pt-6 shadow-sm 2xl:col-span-2">
+      <div className="rounded-lg border px-6 pt-6 shadow-sm 2xl:col-span-2">
         <p className="text-muted-foreground/90 text-start text-xl font-medium">
           Select the appropriate action
         </p>
