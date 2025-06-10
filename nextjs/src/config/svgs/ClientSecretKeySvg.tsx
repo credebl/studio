8edd9f102b43620e3ac05f515c68343b63c45950
pranalidhanavade <pranali.dhanavade@ayanworks.com>
2025-06-10 10:53:43 +0530

    developers settings rendering issues
    Signed-off-by: pranalidhanavade <pranali.dhanavade@ayanworks.com>

--- nextjs/src/config/svgs/ClientSecretKeySvg.tsx
+++ nextjs/src/config/svgs/ClientSecretKeySvg.tsx
@@ -1,4 +1,4 @@
-export const clientSecretKeySvg = (): React.JSX.Element => (
+export const ClientSecretKeySvg = (): React.JSX.Element => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width="36"
