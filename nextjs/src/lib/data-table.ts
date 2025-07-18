489b0f4ae77003d5854828144732596e65aa6c46
pranalidhanavade <pranali.dhanavade@ayanworks.com>
2025-07-18 20:25:05 +0530

    fix: sonarlint issues
    Signed-off-by: pranalidhanavade <pranali.dhanavade@ayanworks.com>

--- nextjs/src/lib/data-table.ts
+++ nextjs/src/lib/data-table.ts
@@ -32,7 +32,7 @@ export function getCommonPinningStyles<TData>({
     right: isPinned === 'right' ? `${column.getAfter('right')}px` : undefined,
     opacity: isPinned ? 0.97 : 1,
     position: isPinned ? 'sticky' : 'relative',
-    background: isPinned ? 'hsl(var(--background))' : 'hsl(var(--background))',
+    background: 'hsl(var(--background))',
     width: column.getSize(),
     zIndex: isPinned ? 1 : 0,
   }
--- nextjs/tailwind.config.js
+++ nextjs/tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [],
+  darkMode: 'class',
   theme: {
     extend: {
       colors: {
