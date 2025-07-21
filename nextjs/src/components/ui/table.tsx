186d4a37594791bedd82cfc9b129bda5366acc2b
pranalidhanavade <pranali.dhanavade@ayanworks.com>
2025-07-21 15:36:23 +0530

    fix: sonarlint issue for table
    Signed-off-by: pranalidhanavade <pranali.dhanavade@ayanworks.com>

--- nextjs/src/components/ui/table.tsx
+++ nextjs/src/components/ui/table.tsx
@@ -1,5 +1,4 @@
 /* NOSONAR */
-
 'use client'
 
 import * as React from 'react'
@@ -12,17 +11,12 @@ function Table({ className, ...props }: React.ComponentProps<'table'>) {
       data-slot="table-container"
       className="relative w-full overflow-x-auto"
     >
+      {/* NOSONAR */}
       <table
         data-slot="table"
         className={cn('w-full caption-bottom text-sm', className)}
         {...props}
-      >
-        {/* <thead className="sr-only">
-          <tr>
-            <th></th>
-          </tr>
-        </thead> */}
-      </table>
+      ></table>
     </div>
   )
 }
