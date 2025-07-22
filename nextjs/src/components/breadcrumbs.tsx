f7704df447a2f1dd3650b2ef6100c844c5b14f95
Sujit <sujit.sutar@ayanworks.com>
2025-07-22 17:00:32 +0530

    fix/breadcrumb issue
    Signed-off-by: Sujit <sujit.sutar@ayanworks.com>

--- nextjs/src/components/breadcrumbs.tsx
+++ nextjs/src/components/breadcrumbs.tsx
@@ -12,6 +12,7 @@ import React, { Fragment, useMemo } from 'react'
 import { usePathname, useSearchParams } from 'next/navigation'
 
 import { IconSlash } from '@tabler/icons-react'
+import { UUID_REGEX } from '@/config/CommonConstant'
 import { useBreadcrumbs } from '@/hooks/use-breadcrumbs'
 
 export function Breadcrumbs(): React.JSX.Element | null {
@@ -20,8 +21,6 @@ export function Breadcrumbs(): React.JSX.Element | null {
   const pathname = usePathname()
   const searchParams = useSearchParams()
   const alias = React.useMemo(() => searchParams.get('alias'), [searchParams])
-  const UUID_REGEX =
-    /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i
   const isOrganizationUUIDPath = useMemo(() => {
     const parts = pathname.split('/').filter(Boolean) // remove empty strings
     return (
@@ -35,21 +34,21 @@ export function Breadcrumbs(): React.JSX.Element | null {
     return null
   }
 
+  function copyToClipboard(text: string): void {
+    window.navigator.clipboard.writeText(text)
+    setCopied(true)
+    setTimeout(() => {
+      setCopied(false)
+    }, 2000)
+  }
+
   return (
     <Breadcrumb>
       <BreadcrumbList>
         {isOrganizationUUIDPath ? (
           <>
             <BreadcrumbItem className="hidden md:block">
-              <BreadcrumbLink
-                onClick={() => {
-                  window.navigator.clipboard.writeText(items[1].title)
-                  setCopied(true)
-                  setTimeout(() => {
-                    setCopied(false)
-                  }, 2000)
-                }}
-              >
+              <BreadcrumbLink onClick={() => copyToClipboard(items[1].title)}>
                 <div className="relative">
                   Overview
                   {copied && (
@@ -86,7 +85,21 @@ export function Breadcrumbs(): React.JSX.Element | null {
                   )}
                   {index === items.length - 1 && (
                     <BreadcrumbPage>
-                      {alias ? alias : decodedTitle}
+                      {alias ? (
+                        <button
+                          className="relative"
+                          onClick={() => copyToClipboard(decodedTitle)}
+                        >
+                          {alias}
+                          {copied && (
+                            <div className="absolute top-7 z-50 ml-2 rounded-md bg-black px-2 py-1 text-xs font-semibold text-white">
+                              Copied!
+                            </div>
+                          )}
+                        </button>
+                      ) : (
+                        decodedTitle
+                      )}
                     </BreadcrumbPage>
                   )}
                 </Fragment>
