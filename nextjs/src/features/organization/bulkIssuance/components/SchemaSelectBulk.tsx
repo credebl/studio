f836c9bcdeb05bf4bf8ec8167d911a93ba075cfb
Sujit <sujit.sutar@ayanworks.com>
2025-08-07 18:30:39 +0530

    fix/issues ecountered during dev testing
    Signed-off-by: Sujit <sujit.sutar@ayanworks.com>

---
+++ nextjs/src/features/organization/bulkIssuance/components/SchemaSelectBulk.tsx
@@ -0,0 +1,39 @@
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
+import { ISelectSchmea } from '../../emailIssuance/type/EmailIssuance'
+import React from 'react'
+
+function SchemaSelectBulk({
+  allSchema,
+  handleFilterChange,
+  optionsWithDefault,
+}: ISelectSchmea): React.JSX.Element {
+  return (
+    <div>
+      <p className="pb-6 text-xl font-semibold opacity-0">Schema Filter</p>
+      <Select
+        defaultValue={"Organization's schema"}
+        value={allSchema ? 'All schemas' : "Organization's schema"}
+        onValueChange={handleFilterChange}
+      >
+        <SelectTrigger className="min-w-lg rounded-lg border p-2.5 text-sm">
+          <SelectValue placeholder="Select schema type" />
+        </SelectTrigger>
+        <SelectContent>
+          {optionsWithDefault.map((opt) => (
+            <SelectItem key={opt} value={opt}>
+              {opt}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+    </div>
+  )
+}
+
+export default SchemaSelectBulk
