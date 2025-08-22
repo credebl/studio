dd6a9a93e0be655872cab4787ecb5b7f989bd58e
alokmore-awts <alok.more@ayanworks.com>
2025-08-22 16:35:46 +0530

    Feat/app launcher implementation (#1160)
    * feat: implementation of app launcher
    
    Signed-off-by: alokmore-awts <alok.more@ayanworks.com>
    
    * Modified changes in appLauncher file
    
    Signed-off-by: alokmore-awts <alok.more@ayanworks.com>
    
    * modified env.sample
    
    Signed-off-by: alokmore-awts <alok.more@ayanworks.com>
    
    ---------
    
    Signed-off-by: alokmore-awts <alok.more@ayanworks.com>

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -20,6 +20,7 @@ const buttonVariants = cva(
         ghost:
           'hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50',
         link: 'text-primary underline-offset-4 hover:underline',
+        hamburger: 'bg-transparent hover:bg-muted-foreground/20',
       },
       size: {
         default: 'h-9 px-4 py-2 has-[>svg]:px-3',
