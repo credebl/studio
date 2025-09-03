8377ac4536e9ff6defd169e04fcc85f0361f5035
pranalidhanavade <pranali.dhanavade@ayanworks.com>
2025-09-04 00:09:44 +0530

    fix: attributes coming empty for w3c schema in verification flow
    Signed-off-by: pranalidhanavade <pranali.dhanavade@ayanworks.com>

--- src/features/verification/components/EmailAttributesSelection.tsx
+++ src/features/verification/components/EmailAttributesSelection.tsx
@@ -1,5 +1,4 @@
 /* eslint-disable max-lines */
-/* eslint-disable @typescript-eslint/no-explicit-any */
 'use client'
 
 import { ISelectedAttributes, NumberAttribute } from '../type/interface'
@@ -49,11 +48,15 @@ const EmailAttributesSelection = (): JSX.Element => {
   const router = useRouter()
   const orgId = useAppSelector((state) => state.organization.orgId)
 
+  const verificationRouteType = useAppSelector(
+    (state) => state.verification.routeType,
+  )
+
   const getSelectedCredDefData = useAppSelector(
     (state) => state.verification.CredDefData,
   )
-  const verificationRouteType = useAppSelector(
-    (state) => state.verification.routeType,
+  const getSelectedW3cSchemaData = useAppSelector(
+    (state) => state.verification.w3cSchemaAttributes,
   )
   const selectedSchemaAttributes = useAppSelector(
     (state) => state.verification.schemaAttributes,
@@ -118,22 +121,6 @@ const EmailAttributesSelection = (): JSX.Element => {
     dispatch(setSelectedAttributeData(updatedAttributes))
   }
 
-  const isInvalidNumberAttribute = (attribute: NumberAttribute): boolean => {
-    const isOptionInvalid =
-      attribute.selectedOption === null ||
-      attribute.selectedOption === '' ||
-      attribute.selectedOption === 'Select'
-    const isValueInvalid = attribute.value === null || attribute.value === ''
-    if (!isOptionInvalid && isValueInvalid) {
-      setErrMsg('Value is missing or is not a number')
-      return true
-    } else if (!isValueInvalid && isOptionInvalid) {
-      setErrMsg('Condition is missing')
-      return true
-    }
-    return false
-  }
-
   const getOrgDetails = async (): Promise<void> => {
     setLoading(true)
 
@@ -164,6 +151,22 @@ const EmailAttributesSelection = (): JSX.Element => {
   useEffect(() => {
     getOrgDetails()
   }, [])
+
+  const isInvalidNumberAttribute = (attribute: NumberAttribute): boolean => {
+    const isOptionInvalid =
+      attribute.selectedOption === null ||
+      attribute.selectedOption === '' ||
+      attribute.selectedOption === 'Select'
+    const isValueInvalid = attribute.value === null || attribute.value === ''
+    if (!isOptionInvalid && isValueInvalid) {
+      setErrMsg('Value is missing or is not a number')
+      return true
+    } else if (!isValueInvalid && isOptionInvalid) {
+      setErrMsg('Condition is missing')
+      return true
+    }
+    return false
+  }
 
   const hasInvalidNumberAttributes = (): boolean => {
     const numberAttributes = attributeData?.filter(
@@ -204,142 +207,94 @@ const EmailAttributesSelection = (): JSX.Element => {
     redirectToAppropriatePage()
   }
 
-  const createAttributeObjects = (
-    schema: any,
-    matchingCredDefs: any[],
-    attributes: {
-      displayName: string
-      attributeName: string
-      schemaDataType: string
-    }[],
-  ): ISelectedAttributes[] =>
-    matchingCredDefs.flatMap((credDef) =>
-      attributes.map((attribute) => ({
-        displayName: attribute.displayName,
-        attributeName: attribute.attributeName,
-        isChecked: false,
-        value: '',
-        condition: '',
-        options: predicatesConditions,
-        dataType: attribute.schemaDataType,
-        schemaName: schema?.schemaId?.split(':')[2] || '',
-        credDefName: credDef.tag,
-        schemaId: schema.schemaId,
-        credDefId: credDef.credentialDefinitionId,
-        selectedOption: 'Select',
-        inputError: '',
-        selectError: '',
-      })),
-    )
-
-  function getAttributesFromSchema(
-    schema: (typeof selectedSchemaAttributes)[number],
-    selectedCredDefs: typeof getSelectedCredDefData,
-  ): ISelectedAttributes[] {
-    if (!schema.attributes || !Array.isArray(schema.attributes)) {
-      return []
-    }
-
-    const getMatchingCredDefs = (
-      selectedCredDefs: any[],
-      schemaId: string,
-    ): any[] =>
-      (Array.isArray(selectedCredDefs)
-        ? selectedCredDefs.filter(
-            (credDef) => credDef.schemaLedgerId === schemaId,
-          )
-        : [])
-
-    const matchingCredDefs = getMatchingCredDefs(
-      selectedCredDefs,
-      schema.schemaId,
-    )
-    return createAttributeObjects(schema, matchingCredDefs, schema.attributes)
-  }
-
-  // Extract attribute processing for a single schema
-  const processSchemaAttributes = (
-    schema: any,
-    selectedCredDefs: any[],
-  ): any[] => {
-    const attributes = []
-    const matchingCredDefs = Array.isArray(selectedCredDefs)
-      ? selectedCredDefs.filter(
-          (credDef) => credDef.schemaLedgerId === schema.schemaId,
-        )
-      : []
-
-    for (const attribute of schema.attributes) {
-      for (const credDef of matchingCredDefs) {
-        attributes.push({
-          displayName: attribute.displayName,
-          attributeName: attribute.attributeName,
-          isChecked: false,
-          value: '',
-          condition: '',
-          options: predicatesConditions,
-          dataType: attribute.schemaDataType,
-          schemaName: schema?.schemaId?.split(':')[2] || '',
-          credDefName: credDef.tag,
-          schemaId: schema.schemaId,
-          credDefId: credDef.credentialDefinitionId,
-          selectedOption: 'Select',
-          inputError: '',
-          selectError: '',
-        })
-      }
-    }
-
-    return attributes
-  }
-
-  // Extract the W3C schema processing logic
-  const processW3CSchemas = (
-    parsedSchemaDetails: any[],
-    selectedCredDefs: any[],
-  ): any[] => {
-    const allAttributes = []
-
-    for (const schema of parsedSchemaDetails) {
-      if (schema.attributes && Array.isArray(schema.attributes)) {
-        const schemaAttributes = processSchemaAttributes(
-          schema,
-          selectedCredDefs,
-        )
-        allAttributes.push(...schemaAttributes)
-      }
-    }
-
-    return allAttributes
-  }
-
   const loadAttributesData = async (): Promise<void> => {
     setLoading(true)
 
     try {
       setAttributeData([])
+      if (w3cSchema) {
+        const parsedW3CSchemaDetails = getSelectedW3cSchemaData
+        if (
+          Array.isArray(parsedW3CSchemaDetails) &&
+          parsedW3CSchemaDetails.length > 0
+        ) {
+          const allAttributes = parsedW3CSchemaDetails.flatMap((schema) => {
+            if (schema.attributes && Array.isArray(schema.attributes)) {
+              return schema.attributes.map((attribute) => ({
+                ...attribute,
+                schemaName: schema.schemaName,
+                credDefName: '',
+                schemaId: schema.schemaId,
+                credDefId: '',
+              }))
+            }
+            return []
+          })
 
-      const selectedCredDefs = getSelectedCredDefData || []
-      const parsedSchemaDetails = Array.isArray(selectedSchemaAttributes)
-        ? selectedSchemaAttributes
-        : []
+          const inputArray = allAttributes.map((attribute) => ({
+            displayName: attribute.displayName,
+            attributeName: attribute.attributeName,
+            isChecked: false,
+            value: '',
+            condition: '',
+            options: predicatesConditions,
+            dataType: attribute.schemaDataType,
+            schemaName: attribute.schemaName ?? '',
+            credDefName: attribute.credDefName,
+            schemaId: attribute.schemaId ?? '',
+            credDefId: attribute.credDefId,
+            selectedOption: 'Select',
+            inputError: '',
+            selectError: '',
+          }))
 
-      if (parsedSchemaDetails.length === 0) {
-        console.error('Parsed schema details are not in the expected format.')
-        return
-      }
-
-      let allAttributes = []
-
-      if (!w3cSchema) {
-        allAttributes = parsedSchemaDetails.flatMap((schema) =>
-          getAttributesFromSchema(schema, selectedCredDefs),
-        )
+          setAttributeData(inputArray)
+        } else {
+          console.error('W3C schema details are not in the expected format.')
+        }
       } else {
-        allAttributes = processW3CSchemas(parsedSchemaDetails, selectedCredDefs)
-      }
+        const selectedCredDefs = getSelectedCredDefData || []
 
-      setAttributeData(allAttributes)
+        const parsedSchemaDetails = Array.isArray(selectedSchemaAttributes)
+          ? selectedSchemaAttributes
+          : []
+        if (
+          Array.isArray(parsedSchemaDetails) &&
+          parsedSchemaDetails.length > 0
+        ) {
+          const allAttributes = parsedSchemaDetails.flatMap((schema) => {
+            if (schema.attributes && Array.isArray(schema.attributes)) {
+              return schema.attributes.flatMap((attribute) => {
+                const matchingCredDefs = Array.isArray(selectedCredDefs)
+                  ? selectedCredDefs.filter(
+                      (credDef) => credDef.schemaLedgerId === schema.schemaId,
+                    )
+                  : []
+                return matchingCredDefs.map((credDef) => ({
+                  displayName: attribute.displayName,
+                  attributeName: attribute.attributeName,
+                  isChecked: false,
+                  value: '',
+                  condition: '',
+                  options: predicatesConditions,
+                  dataType: attribute.schemaDataType,
+                  schemaName: schema?.schemaId?.split(':')[2] || '',
+                  credDefName: credDef.tag,
+                  schemaId: schema.schemaId,
+                  credDefId: credDef.credentialDefinitionId,
+                  selectedOption: 'Select',
+                  inputError: '',
+                  selectError: '',
+                }))
+              })
+            }
+            return []
+          })
+          setAttributeData(allAttributes)
+        } else {
+          console.error('Parsed schema details are not in the expected format.')
+        }
+      }
     } catch (error) {
       console.error('Error fetching data:', error)
     } finally {
