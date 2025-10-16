5f98e38d9e4ef93c1e001e24ec188394f2ed2c62
sujitaw <sujit.sutar@ayanworks.com>
2025-10-16 18:24:52 +0530

    fix/logo change when visited from verifier for sigin in
    Signed-off-by: sujitaw <sujit.sutar@ayanworks.com>
    Signed-off-by: pranalidhanavade <pranali.dhanavade@ayanworks.com>

--- src/features/components/sigin-view.tsx
+++ src/features/components/sigin-view.tsx
@@ -1,11 +1,13 @@
 'use client'
 
 import { CredeblLogoHeight, CredeblLogoWidth } from '@/config/CommonConstant'
+
 import Footer from '@/components/Footer'
 import Image from 'next/image'
 import { Metadata } from 'next'
 import React from 'react'
 import UserAuthForm from './user-auth-form'
+import { useSearchParams } from 'next/navigation'
 import { useTheme } from 'next-themes'
 
 const APP_ENV =
@@ -17,22 +19,36 @@ export const metadata: Metadata = {
 }
 
 export default function SignInPage(): React.JSX.Element {
+  const searchParams = useSearchParams()
+  const alias = searchParams.get('clientAlias')
   const { resolvedTheme } = useTheme()
-
-  const logoImageSrc =
+  let logoImageSrc =
     resolvedTheme === 'dark'
       ? `/logos/${APP_ENV}_logo_dark.svg`
       : `/logos/${APP_ENV}_logo.svg`
 
+  switch (alias) {
+    case 'VERIFIER':
+      logoImageSrc = '/favicon-sovio.ico'
+      break
+
+    default:
+      logoImageSrc =
+        resolvedTheme === 'dark'
+          ? `/logos/${APP_ENV}_logo_dark.svg`
+          : `/logos/${APP_ENV}_logo.svg`
+      break
+  }
+
   return (
     <div className="relative flex min-h-screen flex-col bg-[image:var(--card-gradient)]">
-      <div className="absolute top-1 left-0 z-20 md:top-8 md:left-4">
+      <div className="absolute top-1 left-0 z-20 max-h-24 md:top-8 md:left-4">
         <Image
           height={CredeblLogoHeight}
           width={CredeblLogoWidth}
           alt={`${APP_ENV} Logo`}
           src={logoImageSrc}
-          className="mx-0 h-10 w-fit px-2 md:h-auto md:w-50"
+          className="mx-0 h-10 w-fit object-contain px-2 md:h-20 md:w-50"
         />
       </div>
 
