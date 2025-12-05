7d952bc4ec9dc411016eedea998d258f34bd7a9c
pranalidhanavade <pranali.dhanavade@ayanworks.com>
2025-12-05 12:20:33 +0530

    WIP: oid mockup
    Signed-off-by: pranalidhanavade <pranali.dhanavade@ayanworks.com>

--- src/features/templates/TemplateCreation.tsx
+++ src/features/templates/TemplateCreation.tsx
@@ -1,9 +1,7 @@
 import React from 'react'
 
 const TemplateCreation = () => {
-  return (
-    <div>TemplateCreation</div>
-  )
+  return <div>TemplateCreation</div>
 }
 
 export default TemplateCreation
