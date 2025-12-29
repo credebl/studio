95b29d69fa9eb091dfb97361ddd8001051b3269b
sujitaw <sujit.sutar@ayanworks.com>
2025-12-29 17:10:12 +0530

    fix : login and authentication error on build related to crypto aes (#1266)
    * fix/login issue on build environment
    
    Signed-off-by: sujitaw <sujit.sutar@ayanworks.com>
    
    * fix/pr comments
    
    Signed-off-by: sujitaw <sujit.sutar@ayanworks.com>
    
    ---------
    
    Signed-off-by: sujitaw <sujit.sutar@ayanworks.com>

--- src/app/api/server/encryption.ts
+++ src/app/api/server/encryption.ts
@@ -1,5 +1,8 @@
-import CryptoJS from 'crypto-js'
+import Base64 from 'crypto-js/enc-base64'
+import Hex from 'crypto-js/enc-hex'
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const AES = require('crypto-js/aes')
 export const passwordEncryption = (password: string): string => {
   if (typeof window !== 'undefined') {
     throw new Error('passwordEncryption is server-only')
@@ -9,9 +12,21 @@ export const passwordEncryption = (password: string): string => {
   if (!CRYPTO_PRIVATE_KEY) {
     throw new Error('Missing CRYPTO_PRIVATE_KEY in environment variables')
   }
+  if (!process.env.OPENSSL_HEADER) {
+    throw new Error('Missing OPENSSL_HEADER in environment variables')
+  }
 
-  return CryptoJS.AES.encrypt(
-    JSON.stringify(password),
-    CRYPTO_PRIVATE_KEY,
-  ).toString()
+  const encrypted = AES.encrypt(password, CRYPTO_PRIVATE_KEY)
+
+  if (encrypted.salt) {
+    const saltedHeader = Hex.parse(process.env.OPENSSL_HEADER)
+
+    const combined = saltedHeader
+      .concat(encrypted.salt)
+      .concat(encrypted.ciphertext)
+
+    return Base64.stringify(combined)
+  }
+
+  return encrypted.toString(Base64)
 }
